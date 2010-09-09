a6d373b677fb68db2833c1c79c03873e1b51d2fd
haarlaender <haarlaender@8183283b-e642-0410-b1e5-cb06ff2f27ea>
2010-09-09 14:50:49 +0000

    Collection Quick Search

---
+++ faces_presentation-4.0/src/main/java/de/mpg/imeji/collection/CollectionsSearchResultBean.java
@@ -0,0 +1,94 @@
+package de.mpg.imeji.collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import de.mpg.imeji.beans.SessionBean;
+import de.mpg.imeji.beans.SuperContainerBean;
+import de.mpg.imeji.util.BeanHelper;
+import de.mpg.imeji.vo.util.ImejiFactory;
+import de.mpg.jena.controller.CollectionController;
+import de.mpg.jena.controller.SearchCriterion;
+import de.mpg.jena.controller.SortCriterion;
+import de.mpg.jena.controller.SearchCriterion.Filtertype;
+import de.mpg.jena.controller.SearchCriterion.ImejiNamespaces;
+import de.mpg.jena.controller.SearchCriterion.Operator;
+import de.mpg.jena.controller.SortCriterion.SortOrder;
+import de.mpg.jena.vo.CollectionImeji;
+
+public class CollectionsSearchResultBean extends SuperContainerBean<ViewCollectionBean>
+{
+  
+    private int totalNumberOfRecords;
+    private SessionBean sb;
+  
+    private String query;
+    
+
+    public CollectionsSearchResultBean()
+    {
+        super();
+        this.sb = (SessionBean)BeanHelper.getSessionBean(SessionBean.class);
+       
+        
+    }
+
+    @Override
+    public String getNavigationString()
+    {
+        return "pretty:collections";
+    }
+
+    @Override
+    public int getTotalNumberOfRecords()
+    {
+       return totalNumberOfRecords;
+    }
+
+    @Override
+    public List<ViewCollectionBean> retrieveList(int offset, int limit)
+    {
+        CollectionController controller = new CollectionController(sb.getUser());
+        Collection<CollectionImeji> collections = new ArrayList<CollectionImeji>();
+        
+        List<SearchCriterion> scList = new ArrayList<SearchCriterion>();
+        scList.add(new SearchCriterion(ImejiNamespaces.CONTAINER_METADATA_TITLE, getQuery()));
+        scList.add(new SearchCriterion(Operator.OR, ImejiNamespaces.CONTAINER_METADATA_DESCRIPTION, getQuery(), Filtertype.REGEX));
+        try
+        {
+            collections = controller.search(scList, null, -1, offset);
+            totalNumberOfRecords = collections.size();
+            logger.info("Found " + totalNumberOfRecords + "collections");
+            
+            SortCriterion sortCriterion = new SortCriterion();
+            sortCriterion.setSortingCriterion(ImejiNamespaces.valueOf(getSelectedSortCriterion()));
+            sortCriterion.setSortOrder(SortOrder.valueOf(getSelectedSortOrder()));
+        
+        
+      
+            collections = controller.search(scList, sortCriterion, limit, offset);
+        }
+        catch (Exception e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        
+        return ImejiFactory.collectionListToBeanList(collections);
+    }
+
+    public void setQuery(String query)
+    {
+        this.query = query;
+    }
+
+    public String getQuery()
+    {
+        return query;
+    }
+
+
+   
+}
---
+++ faces_presentation-4.0/src/main/java/de/mpg/imeji/search/QuickSearchBean.java
@@ -0,0 +1,43 @@
+package de.mpg.imeji.search;
+
+import de.mpg.imeji.collection.CollectionsSearchResultBean;
+import de.mpg.imeji.util.BeanHelper;
+
+public class QuickSearchBean
+{
+    
+    private String searchString;
+    private String selectedSearchType = "collections";
+
+    public void setSearchString(String searchString)
+    {
+        this.searchString = searchString;
+    }
+
+    public String getSearchString()
+    {
+        return searchString;
+    }
+
+    public void setSelectedSearchType(String selectedSearchType)
+    {
+        this.selectedSearchType = selectedSearchType;
+    }
+
+    public String getSelectedSearchType()
+    {
+        return selectedSearchType;
+    }
+    
+    public String search()
+    {
+        if(getSelectedSearchType().equals("collections"))
+        {
+            CollectionsSearchResultBean bean = (CollectionsSearchResultBean)BeanHelper.getSessionBean(CollectionsSearchResultBean.class);
+            bean.setQuery(searchString);
+            return "pretty:collectionsSearchResults";
+        }
+        return "pretty:";
+    }
+    
+}

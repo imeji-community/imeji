56c7ceffa8b27708d07fe94dee2956d2bae27a92
yu <yu@8183283b-e642-0410-b1e5-cb06ff2f27ea>
2011-02-09 17:40:18 +0000

    Quadratisch Thumbnail Resolution

--- faces_presentation-4.0/src/main/java/de/mpg/imeji/upload/helper/ImageHelper.java
+++ faces_presentation-4.0/src/main/java/de/mpg/imeji/upload/helper/ImageHelper.java
@@ -1,21 +1,29 @@
 package de.mpg.imeji.upload.helper;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
+
 import javax.imageio.ImageIO;
 import javax.naming.InitialContext;
+
 import org.ajax4jsf.resource.image.animatedgif.GifDecoder;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.InputStreamRequestEntity;
 import org.apache.commons.httpclient.methods.PutMethod;
 import org.apache.xmlbeans.XmlCursor;
+
+import com.sun.mail.handlers.image_gif;
+
 import de.escidoc.schemas.components.x09.ComponentDocument.Component;
 import de.escidoc.schemas.components.x09.ComponentDocument.Component.Content.Storage.Enum;
 import de.escidoc.schemas.components.x09.PropertiesDocument.Properties;
@@ -121,18 +129,42 @@ public class ImageHelper{
         return item;
     } 
     
+//    public static BufferedImage scaleImage(BufferedImage image, int size) throws Exception{
+//    	int width = image.getWidth();
+//    	int height = image.getHeight();
+//    	Image rescaledImage;
+//    	if(width > height)
+//    		rescaledImage = image.getScaledInstance(size, -1, Image.SCALE_SMOOTH);
+//    	else
+//    		rescaledImage = image.getScaledInstance(-1, size, Image.SCALE_SMOOTH);
+//        BufferedImage rescaledBufferedImage = new BufferedImage(rescaledImage.getWidth(null), rescaledImage.getHeight(null), BufferedImage.TYPE_INT_RGB);
+//        Graphics g = rescaledBufferedImage.getGraphics();
+//        g.drawImage(rescaledImage, 0, 0, null);
+//        return rescaledBufferedImage;
+//    }
+    
     public static BufferedImage scaleImage(BufferedImage image, int size) throws Exception{
-    	int width = image.getWidth();
-    	int height = image.getHeight();
-    	Image rescaledImage;
+    	int width = image.getWidth(null);
+    	int height = image.getHeight(null);
+    	BufferedImage newImg = null;
     	if(width > height)
-    		rescaledImage = image.getScaledInstance(size, -1, Image.SCALE_SMOOTH);
+    	{
+    		newImg= new BufferedImage(height, height,image.getType());
+        	Graphics g1 = newImg.createGraphics();
+        	g1.drawImage(image, (height-width)/2, 0, null);
+    	}
     	else
-    		rescaledImage = image.getScaledInstance(-1, size, Image.SCALE_SMOOTH);
-        BufferedImage rescaledBufferedImage = new BufferedImage(rescaledImage.getWidth(null), rescaledImage.getHeight(null), BufferedImage.TYPE_INT_RGB);
-        Graphics g = rescaledBufferedImage.getGraphics();
-        g.drawImage(rescaledImage, 0, 0, null);
-        return rescaledBufferedImage;
+    	{
+    		newImg= new BufferedImage(width, width,image.getType());
+        	Graphics g1 = newImg.createGraphics();
+        	g1.drawImage(image, 0, (width-height)/2, null);
+    	}
+    	Image rescaledImage;
+    	rescaledImage = newImg.getScaledInstance(size, -1, Image.SCALE_SMOOTH);
+        BufferedImage rescaledBufferedImage = new BufferedImage(rescaledImage.getWidth(null), rescaledImage.getHeight(null), newImg.getType());
+        Graphics g2 = rescaledBufferedImage.getGraphics();
+        g2.drawImage(rescaledImage, 0, 0, null);
+        return newImg;
     }
     
     public static GifDecoder checkAnimation(byte[] image) throws Exception{
@@ -222,68 +254,5 @@ public class ImageHelper{
         return PropertyReader.getProperty("xsd.metadata.content-category.original-resolution");
     }
     
-
-    /**
-     * for reading CMYK images
-     * Creates new RGB images from all the CMYK images passed
-     * in on the command line.
-     * 
-     */
-//    public static BufferedImage cmykRasterToSRGB(byte[] inputStream, String format)throws Exception{
-//  	  //Find a suitable ImageReader
-//        Iterator readers = ImageIO.getImageReadersByFormatName(format);
-//        ImageReader reader = null;
-//        while(readers.hasNext()) {
-//            reader = (ImageReader)readers.next();
-//            if(reader.canReadRaster()) {
-//                break;
-//            }
-//        }
-//        //Stream the image file (the original CMYK image)
-//        ImageInputStream input = ImageIO.createImageInputStream(new ByteArrayInputStream(inputStream));
-//        reader.setInput(input); 
-//        // Create the image.
-//        BufferedImage image;
-//        Raster raster = reader.readRaster(0, null); 
-//	    // Arbitrarily select a BufferedImage type.
-//        int imageType;
-//        switch(raster.getNumBands()) 
-//        {
-//        case 1:
-//        	imageType = BufferedImage.TYPE_BYTE_GRAY;
-//            break;
-//            case 3:
-//            	imageType = BufferedImage.TYPE_3BYTE_BGR;
-//                break;
-//                case 4:
-//                	imageType = BufferedImage.TYPE_4BYTE_ABGR;
-//                	break;
-//                	default:
-//                		throw new UnsupportedOperationException();
-//            }
-//        // Create a BufferedImage.
-//        image = new BufferedImage(raster.getWidth(),raster.getHeight(),imageType);
-//        // Set the image data.
-//        image.getRaster().setRect(raster);
-//    	return image;
-//    }
-//    
-//    public static BufferedImage readCMYKwithJAI(byte[] inputStream, String format)throws Exception
-//    {
-//    	ByteArrayInputStream bais = new ByteArrayInputStream(inputStream);
-//    	SeekableStream seekableStream = SeekableStream.wrapInputStream(bais,false);
-//    	PlanarImage src = JAI.create("Stream", seekableStream);
-//    	BufferedImage image = src.getAsBufferedImage();    	
-//    	return image;
-//    }
-//    
-//    public static BufferedImage readCMYKwithjm4java(byte[] inputStream, String format)throws Exception
-//    {
-//    	ByteArrayInputStream bais = new ByteArrayInputStream(inputStream);
-//    	Stream2BufferedImage stream4Image= new Stream2BufferedImage();
-//    	stream4Image.consumeOutput(bais);
-//    	BufferedImage image = stream4Image.getImage();
-//    	return image;
-//    }
 
 }

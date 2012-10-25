11aad6859bfb7a04f40200133b464a291227986d
Bastien Saquet <saquet@mpdl.mpg.de>
2012-10-25 18:08:52 +0200

    Revert "Ingest architecture implemented"
    This reverts commit 580e2729c44aa4c0c16e196599ee8008843ae0d8.

--- imeji_presentation/src/main/java/de/mpg/imeji/logic/ingest/validator/ProfileValidator.java
+++
@@ -1,9 +0,0 @@
-package de.mpg.imeji.logic.ingest.validator;
-
-public class ProfileValidator
-{
-    public void valid(String profileXml)
-    {
-        // if not valid, through exception
-    }
-}

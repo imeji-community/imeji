2a65d183873b799e9fe7aad2038eb825160394f5
Bastien Saquet <saquet@mpdl.mpg.de>
2013-02-14 10:51:30 +0100

    Fix search test error

--- imeji_presentation/src/test/java/search/URLQueryTransformerTest.java
+++ imeji_presentation/src/test/java/search/URLQueryTransformerTest.java
@@ -51,7 +51,7 @@ public class URLQueryTransformerTest
      * TODO Non working characters: ()+=
      */
     private static String specialsChar = "japanese:テスト  chinese:實驗 yiddish:פּראָבע arab:اختبار bengali: পরীক্ষা other:öäü@ß$&@*~!?{}[]-#'.,áò";
-    private static String advancedQuery = "(col==\"http://imeji.org/collection/86\" AND (e6537a19-86b6-47ca-bba7-c8cc4d6bd6bc:text=\"TEST\") OR (750c1b37-f766-4b74-9d83-ddc858ff4365:title=\"TEST\") OR (rdfType==\"http://imeji.org/terms/metadata#number\"))";
+    private static String advancedQuery = "(col==\"http://imeji.org/collection/86\" AND (e6537a19-86b6-47ca-bba7-c8cc4d6bd6bc:text=\"TEST\") OR (750c1b37-f766-4b74-9d83-ddc858ff4365:title=\"TEST\") OR (type==\"http://imeji.org/terms/metadata#number\"))";
     private static String simpleQuery = "TEST";
 
     /**

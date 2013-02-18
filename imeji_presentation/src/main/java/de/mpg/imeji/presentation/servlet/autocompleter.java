da966da26831ad10f539781d7380ac6bf894cfd9
Ye Cao <cao@mpdl.mpg.de>
2013-02-18 12:56:56 +0100

    auto-complete json parser works for google api and pubman name service

--- imeji_presentation/src/main/java/de/mpg/imeji/presentation/servlet/autocompleter.java
+++ imeji_presentation/src/main/java/de/mpg/imeji/presentation/servlet/autocompleter.java
@@ -3,7 +3,9 @@ package de.mpg.imeji.presentation.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringReader;
+import java.io.StringWriter;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,6 +15,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
 /**
  * Servlet implementation class autocompleter
@@ -41,15 +46,32 @@ public class autocompleter extends HttpServlet {
 		String suggest = request.getParameter("term");
 		String datasource = request.getParameter("datasource");
 		String responseString = "";
+		// test data
+		JSONObject obj1 = new JSONObject();
+		obj1.put("label", "yeye");
+		obj1.put("value", new Integer(100));
+		obj1.put("anytype", new Integer(100));
+		JSONObject obj2 = new JSONObject();
+		obj2.put("label", "foo33");
+		obj2.put("value", new Integer(110));
+		obj2.put("anytype", new Integer(100));
+		JSONArray list = new JSONArray();
+		list.add(obj1);
+		list.add(obj2);
+		StringWriter out1 = new StringWriter();
+		list.writeJSONString(out1);
+		String jsonText = out1.toString();
+
 		if (suggest.toString().isEmpty()) {
 			suggest = "a";
 		} else if (!suggest.toString().isEmpty()) {
 			try {
 				HttpClient client = new HttpClient();
 				GetMethod getMethod = new GetMethod(datasource
-								+ URLEncoder.encode(suggest.toString(), "UTF-8"));
+						+ URLEncoder.encode(suggest.toString(), "UTF-8"));
 				client.executeMethod(getMethod);
 				responseString = getMethod.getResponseBodyAsString().trim();
+				responseString = passResult(responseString, datasource);
 			} catch (Exception e) {
 				throw new RuntimeException(e);
 			}
@@ -57,125 +79,139 @@ public class autocompleter extends HttpServlet {
 		response.setContentType("application/json");
 		PrintWriter out = response.getWriter();
 		out.print(formatResultString(responseString));
+		// out.print(jsonText);
 		out.flush();
 	}
-	 private String formatResultString(String s)
-     {
-         if (s.contains("\"formatted_address\" :"))
-         {
-             // is https://developers.google.com/maps/documentation/geocoding
-             try
-             {
-                 s = parseMapsGoogleApiGeo(s);
-             }
-             catch (Exception e)
-             {
-                 throw new RuntimeException("Error parsing google geo api results", e);
-             }
-         }
-         if (s.contains("\"status\" : \"ZERO_RESULTS\""))
-         {
-             // google zero result
-             s = "";
-         }
-         if (!s.startsWith("["))
-         {
-             s = "[ " + s;
-         }
-         if (!s.endsWith("]"))
-         {
-             s = s + "]";
-         }
-         return s;
-     }
 
-     private String parseMapsGoogleApiGeo(String str) throws Exception
-     {
-         String response = "";
-         String address = "";
-         String latitude = "";
-         String longitude = "";
-         StringReader reader = new StringReader(str);
-         int c = 0;
-         boolean readingAddress = false;
-         boolean readingLongitude = false;
-         boolean readingLatitude = false;
-         boolean readingLocation = false;
-         String buffer = "";
-         while ((c = reader.read()) != -1)
-         {
-             buffer += (char)c;
-             if (buffer.contains("\"formatted_address\" : \""))
-             {
-                 // start reading address
-                 readingAddress = true;
-                 buffer = "";
-             }
-             else if (readingAddress && c == '"')
-             {
-                 // stop reading address
-                 readingAddress = false;
-                 buffer = "";
-             }
-             else if (buffer.contains("\"location\" : "))
-             {
-                 // Enter in Location group
-                 readingLocation = true;
-                 buffer = "";
-             }
-             else if (readingLocation && c == '}')
-             {
-                 // Sort out Location group
-                 readingLocation = false;
-                 buffer = "";
-             }
-             else if (readingLocation && buffer.contains("\"lat\" : "))
-             {
-                 // start reading latitude
-                 readingLatitude = true;
-                 buffer = "";
-             }
-             else if (readingLatitude && c == ',')
-             {
-                 // stop reading latitude
-                 readingLatitude = false;
-                 buffer = "";
-             }
-             else if (readingLocation && buffer.contains("\"lng\" : "))
-             {
-                 // start reading longitude
-                 readingLongitude = true;
-                 buffer = "";
-             }
-             else if (readingLongitude && c == '\n')
-             {
-                 // stop reading longitude
-                 readingLongitude = false;
-                 buffer = "";
-             }
-             else if (readingAddress)
-             {
-                 address += (char)c;
-             }
-             else if (readingLongitude)
-             {
-                 longitude += (char)c;
-             }
-             else if (readingLatitude)
-             {
-                 latitude += (char)c;
-             }
-             else if (address != "" && latitude != "" && longitude != "" && !readingAddress && !readingLocation)
-             {
-                 // write results
-                 response += "{\"label\" : \"" + address + "\" , \"value\" : \"" + longitude
-                         + "\" , \"latitude\" : \"" + latitude + "\"}, ";
-                 address = "";
-                 latitude = "";
-                 longitude = "";
-             }
-         }
-         return "[" + response + "]";
-     }
+	private String formatResultString(String s) {
+		if (s.contains("\"formatted_address\" :")) {
+			// is https://developers.google.com/maps/documentation/geocoding
+			try {
+				s = parseMapsGoogleApiGeo(s);
+			} catch (Exception e) {
+				throw new RuntimeException(
+						"Error parsing google geo api results", e);
+			}
+		}
+		if (s.contains("\"status\" : \"ZERO_RESULTS\"")) {
+			// google zero result
+			s = "";
+		}
+		if (!s.startsWith("[")) {
+			s = "[ " + s;
+		}
+		if (!s.endsWith("]")) {
+			s = s + "]";
+		}
+		return s;
+	}
 
+	private String parseMapsGoogleApiGeo(String str) throws Exception {
+		String response = "";
+		String address = "";
+		String latitude = "";
+		String longitude = "";
+		StringReader reader = new StringReader(str);
+		int c = 0;
+		boolean readingAddress = false;
+		boolean readingLongitude = false;
+		boolean readingLatitude = false;
+		boolean readingLocation = false;
+		String buffer = "";
+		while ((c = reader.read()) != -1) {
+			buffer += (char) c;
+			if (buffer.contains("\"formatted_address\" : \"")) {
+				// start reading address
+				readingAddress = true;
+				buffer = "";
+			} else if (readingAddress && c == '"') {
+				// stop reading address
+				readingAddress = false;
+				buffer = "";
+			} else if (buffer.contains("\"location\" : ")) {
+				// Enter in Location group
+				readingLocation = true;
+				buffer = "";
+			} else if (readingLocation && c == '}') {
+				// Sort out Location group
+				readingLocation = false;
+				buffer = "";
+			} else if (readingLocation && buffer.contains("\"lat\" : ")) {
+				// start reading latitude
+				readingLatitude = true;
+				buffer = "";
+			} else if (readingLatitude && c == ',') {
+				// stop reading latitude
+				readingLatitude = false;
+				buffer = "";
+			} else if (readingLocation && buffer.contains("\"lng\" : ")) {
+				// start reading longitude
+				readingLongitude = true;
+				buffer = "";
+			} else if (readingLongitude && c == '\n') {
+				// stop reading longitude
+				readingLongitude = false;
+				buffer = "";
+			} else if (readingAddress) {
+				address += (char) c;
+			} else if (readingLongitude) {
+				longitude += (char) c;
+			} else if (readingLatitude) {
+				latitude += (char) c;
+			} else if (address != "" && latitude != "" && longitude != ""
+					&& !readingAddress && !readingLocation) {
+				// write results
+				response += "{\"label\" : \"" + address + "\" , \"value\" : \""
+						+ longitude + "\" , \"latitude\" : \"" + latitude
+						+ "\"}, ";
+				address = "";
+				latitude = "";
+				longitude = "";
+			}
+		}
+		return "[" + response + "]";
+	}
+
+	private String passResult(String s, String source) throws IOException {
+		System.out.println(source);
+		if (source.equals(nameUri)) {
+			Object obj = JSONValue.parse(s);
+			JSONArray array = (JSONArray) obj;
+			JSONArray result = new JSONArray();
+			for (int i = 0; i < array.size(); ++i) {
+				JSONObject object = (JSONObject) array.get(i);
+				// JQuery UI auto-complete required format: array or label,
+				// value pair
+				object.put("label",
+						object.get("http_xmlns_com_foaf_0_1_family_name"));
+				object.put("value",
+						object.get("http_xmlns_com_foaf_0_1_family_name"));
+				result.add(object);
+			}
+			StringWriter out = new StringWriter();
+			result.writeJSONString(out);
+			String jsonText = out.toString();
+			return jsonText;
+
+		} else if (source.equals(leoUri)) {
+			JSONObject obj = (JSONObject) JSONValue.parse(s);
+			JSONArray array = (JSONArray) obj.get("results");
+			JSONArray result = new JSONArray();
+			for (int i = 0; i < array.size(); ++i) {
+				JSONObject object = (JSONObject) array.get(i);
+				// JQuery UI auto-complete required format: array or label,
+				// value pair
+				object.put("label", object.get("formatted_address"));
+				object.put("value", object.get("formatted_address"));
+				result.add(object);
+			}
+			StringWriter out = new StringWriter();
+			result.writeJSONString(out);
+			String jsonText = out.toString();
+			return jsonText;
+
+		}
+		return s;
+
+	}
 }
--- imeji_presentation/src/main/webapp/resources/js/autocomplete.js
+++ imeji_presentation/src/main/webapp/resources/js/autocomplete.js
@@ -1,80 +1,58 @@
 var datasourceUrl;
-function getDatasourceUrl(url){
-	datasourceUrl=url
+function getDatasourceUrl(url) {
+	datasourceUrl = url
 }
 
+$(function() {	function split(val) {
+		return val.split(/,\s*/);
+	}
+	function extractLast(term) {
+		return split(term).pop();
+	}
+	// @Ye: Selector for adding auto-complete ability, currently add to all
+	// input text with class "xHuge_txtInput"
+	/*
+	 * http://net.tutsplus.com/tutorials/javascript-ajax/how-to-use-the-jquery-ui-autocomplete-widget/
+	 * http://api.jquery.com/jQuery.getJSON/
+	 * http://api.jqueryui.com/autocomplete/#option-source
+	 */
+	$(".xHuge_txtInput")
+	// don't navigate away from the field on tab when selecting an item
+	.bind(
+			"keydown",
+			function(event) {
+				if (event.keyCode === $.ui.keyCode.TAB
+						&& $(this).data("autocomplete").menu.active) {
+					event.preventDefault();
+				}
+			}).autocomplete({
+		source : function(request, response) {
+			$.getJSON("/imeji/autocompleter", {
+				term : extractLast(request.term),
+				datasource : datasourceUrl
+			}, function(jsonData) {
+				response(jsonData)
 
-$(function() {
-	//test data
-	var availableTags = [
-		      "ActionScript",
-		      "AppleScript",
-		      "Asp",
-		      "BASIC",
-		      "C",
-		      "C++",
-		      "Clojure",
-		      "COBOL",
-		      "ColdFusion",
-		      "Erlang",
-		      "Fortran",
-		      "Groovy",
-		      "Haskell",
-		      "Java",
-		      "JavaScript",
-		      "Lisp",
-		      "Perl",
-		      "PHP",
-		      "Python",
-		      "Ruby",
-		      "Scala",
-		      "Scheme"
-		    ];
-    function split( val ) {
-      return val.split( /,\s*/ );
-    }
-    function extractLast( term ) {
-      return split( term ).pop();
-    }
- //@Ye: Selector for adding auto-complete ability, currently add to all input text with class "xHuge_txtInput"
-    /*
-     * http://net.tutsplus.com/tutorials/javascript-ajax/how-to-use-the-jquery-ui-autocomplete-widget/
-     * http://api.jquery.com/jQuery.getJSON/
-     * http://api.jqueryui.com/autocomplete/#option-source
-     */
-    $( ".xHuge_txtInput" )
-      // don't navigate away from the field on tab when selecting an item
-      .bind( "keydown", function( event ) {
-        if ( event.keyCode === $.ui.keyCode.TAB &&
-            $( this ).data( "autocomplete" ).menu.active ) {
-          event.preventDefault();
-        }
-      })
-      .autocomplete({
-        source: function( request, response ) {
-        	$.getJSON( "http://localhost:8888/imeji/autocompleter", {
-        		 term: extractLast( request.term ),datasource:datasourceUrl
-              }, function(data){
-            	  console.log("Bob!")
-            	 
-              	 }
-              );
-        	//response(data) 
-        	
-          },
-        search: function() {       
-          // custom minLength, currently start query after entering 2 characters,
-          var term = extractLast( this.value );
-          if ( term.length < 2 ) {
-            return false;
-          }
-        },
-        focus: function() {
-          // prevent value inserted on focus
-          return false;
-        },
-        select: function( event, ui ) {
-           return;        
-        }
-      });
-  });
+			});
+		},
+		search : function() {
+			// TODO if input fields others than expected, abort search
+			// if(!this.id.contains(locationname)||!this.id.contains(familyname){return
+			// false})
+
+			// custom minLength, currently start query after entering 2
+			// characters,
+			var term = extractLast(this.value);
+			if (term.length < 2) {
+				return false;
+			}
+		},
+		focus : function() {
+			// prevent value inserted on focus
+			return false;
+		},
+		select : function(event, ui) {
+			return;
+		}
+	});
+});

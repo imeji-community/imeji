4859a691d46dabdba1d6d75684816583fe301278
Bastien Saquet <saquet@mpdl.mpg.de>
2013-05-24 09:12:19 +0200

    Iso639: fallback when Cone down

--- src/main/java/de/mpg/imeji/presentation/lang/Iso639_1Helper.java
+++ src/main/java/de/mpg/imeji/presentation/lang/Iso639_1Helper.java
@@ -14,17 +14,32 @@ import org.apache.log4j.Logger;
 
 import de.mpg.imeji.presentation.util.PropertyReader;
 
+/**
+ * Utility class for Iso638_1 languages vocabulary
+ * 
+ * @author saquet (initial creation)
+ * @author $Author$ (last modification)
+ * @version $Revision$ $LastChangedDate$
+ */
 public class Iso639_1Helper
 {
     private static Logger logger = Logger.getLogger(Iso639_1Helper.class);
     private List<SelectItem> list = null;
 
+    /**
+     * Default constructor
+     */
     public Iso639_1Helper()
     {
         list = new ArrayList<SelectItem>();
         parseVocabularyString(getVocabularyString());
     }
 
+    /**
+     * Get the Iso638_1 languages vocabulary from CoNe with the options format
+     * 
+     * @return
+     */
     private String getVocabularyString()
     {
         try
@@ -38,24 +53,48 @@ public class Iso639_1Helper
         catch (Exception e)
         {
             logger.error("Couldn't read ISO639_1 vocabulary, will use default one! Error: " + e);
-            return "en|en - English\nde|de - German";
+            return null;
         }
     }
 
+    /**
+     * Parse the Result as defined in Cone
+     * 
+     * @param v
+     */
     private void parseVocabularyString(String v)
     {
-        for (String l : v.split("\n"))
+        try
         {
-            String[] s = l.split("\\|");
-            list.add(new SelectItem(s[0], s[1]));
+            for (String l : v.split("\n"))
+            {
+                String[] s = l.split("\\|");
+                list.add(new SelectItem(s[0], s[1]));
+            }
+        }
+        catch (Exception e)
+        {
+            list = new ArrayList<SelectItem>();
+            list.add(new SelectItem("en", "en - English"));
+            list.add(new SelectItem("de", "de - German"));
         }
     }
 
+    /**
+     * getter
+     * 
+     * @return
+     */
     public List<SelectItem> getList()
     {
         return list;
     }
 
+    /**
+     * setter
+     * 
+     * @param list
+     */
     public void setList(List<SelectItem> list)
     {
         this.list = list;

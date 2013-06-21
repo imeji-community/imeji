38f92a5dc743ae9483cd07cb5dffa955fec7dcdc
Bastien Saquet <saquet@mpdl.mpg.de>
2013-06-21 17:07:24 +0200

    minor cleaning in ingest

--- src/main/java/de/mpg/imeji/logic/ingest/jaxb/JaxbGenericObject.java
+++ src/main/java/de/mpg/imeji/logic/ingest/jaxb/JaxbGenericObject.java
@@ -16,51 +16,54 @@ import de.mpg.imeji.logic.ingest.util.ImejiSchemaFilename;
 /**
  * @author hnguyen
  * @param <T>
- *
  */
-public class JaxbGenericObject<T> extends JaxbUtil implements IJaxbGenericObject<T> {
+public class JaxbGenericObject<T> extends JaxbUtil implements IJaxbGenericObject<T>
+{
+    protected String xsdFilename = ImejiSchemaFilename.IMEJI_INGEST_PROFILE_XSDFILE;
+    protected Class<T> type;
 
-	protected String xsdFilename = ImejiSchemaFilename.IMEJI_INGEST_PROFILE_XSDFILE;
-
-	protected Class<T> type;	
-	
-    public JaxbGenericObject(Class<T> type) {
-         this.type = type;
+    public JaxbGenericObject(Class<T> type)
+    {
+        this.type = type;
     }
-    
-    public JaxbGenericObject(Class<T> type, String xsdFilename) {
+
+    public JaxbGenericObject(Class<T> type, String xsdFilename)
+    {
         this.type = type;
         this.xsdFilename = xsdFilename;
-   }
+    }
 
-    public Class<T> getClassType() {
+    public Class<T> getClassType()
+    {
         return this.type;
     }
-	
-	public String getXsdFilename() {
-		return this.xsdFilename;
-	}
 
-	@Override
-	public void marshal(String xmlFilename, T t) throws JAXBException,
-			SAXException, FileNotFoundException {
-		super.marshal(this.getXsdFilename(), xmlFilename, t);
-	}
+    public String getXsdFilename()
+    {
+        return this.xsdFilename;
+    }
 
-	@Override
-	public T unmarshal(String xmlFilename) throws JAXBException, SAXException {		
-		return super.unmarshal(this.getXsdFilename(), xmlFilename, this.getClassType() );
-	}
+    @Override
+    public void marshal(String xmlFilename, T t) throws JAXBException, SAXException, FileNotFoundException
+    {
+        super.marshal(this.getXsdFilename(), xmlFilename, t);
+    }
 
-	@Override
-	public void marshal(File xmlFile, T t) throws JAXBException, SAXException,
-			FileNotFoundException {
-		super.marshal(this.getXsdFilename(), xmlFile, t);
-	}
+    @Override
+    public T unmarshal(String xmlFilename) throws JAXBException, SAXException
+    {
+        return super.unmarshal(this.getXsdFilename(), xmlFilename, this.getClassType());
+    }
 
-	@Override
-	public T unmarshal(File xmlFile) throws JAXBException, SAXException {
-		return unmarshal(this.getXsdFilename(), xmlFile, this.getClassType());
-	}
+    @Override
+    public void marshal(File xmlFile, T t) throws JAXBException, SAXException, FileNotFoundException
+    {
+        super.marshal(this.getXsdFilename(), xmlFile, t);
+    }
 
+    @Override
+    public T unmarshal(File xmlFile) throws JAXBException, SAXException
+    {
+        return unmarshal(this.getXsdFilename(), xmlFile, this.getClassType());
+    }
 }

d65b78220e1024bc4ffd0373ff000bf0b852bb8d
Bastien Saquet <saquet@mpdl.mpg.de>
2013-06-25 19:05:07 +0200

    File servlet: redirect to correct url when session is null

--- src/main/java/de/mpg/imeji/presentation/servlet/FileServlet.java
+++ src/main/java/de/mpg/imeji/presentation/servlet/FileServlet.java
@@ -13,7 +13,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.axis.transport.http.HTTPConstants;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.http.protocol.HTTP;
 import org.apache.log4j.Logger;
+import org.opensaml.ws.security.provider.HTTPRule;
 
 import de.mpg.imeji.logic.search.Search;
 import de.mpg.imeji.logic.search.Search.SearchType;
@@ -72,21 +77,31 @@ public class FileServlet extends HttpServlet
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
         String url = req.getParameter("id");
-        if (url == null)
+        if (req.getRequestedSessionId() != null)
         {
-            // if the id parameter is null, interpret the whole url as a direct to the file (can only work if the
-            // internal storage is used)
-            url = domain + req.getRequestURI();
-        }
-        resp.setContentType(StorageUtils.getMimeType(StringHelper.getFileExtension(url)));
-        SessionBean session = getSession(req);
-        if (security.check(OperationsType.READ, getUser(session), loadCollection(url, session)))
-        {
-            storageController.read(url, resp.getOutputStream(), false);
+            if (url == null)
+            {
+                // if the id parameter is null, interpret the whole url as a direct to the file (can only work if the
+                // internal storage is used)
+                url = domain + req.getRequestURI();
+            }
+            resp.setContentType(StorageUtils.getMimeType(StringHelper.getFileExtension(url)));
+            SessionBean session = getSession(req);
+            if (security.check(OperationsType.READ, getUser(session), loadCollection(url, session)))
+            {
+                storageController.read(url, resp.getOutputStream(), false);
+            }
+            else
+            {
+                resp.sendError(403, "imeji security: You are not allowed to view this file");
+            }
         }
         else
         {
-            resp.sendError(403, "imeji security: You are not allowed to view this file");
+            // Avoid to reset the session. If the session in request has no id, this means there has been a problem
+            // (because of some redirect?). To avoid the session to be killed, this make a redirect to the correct page,
+            // where the session should be non null
+            resp.sendRedirect(req.getRequestURL().toString() + "?id=" + url);
         }
     }
 

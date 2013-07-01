292d4972e13f0405d4b9a12fde138e6b29ffcec1
Bastien Saquet <saquet@mpdl.mpg.de>
2013-07-01 14:23:37 +0200

    Batch edit: add clone for person and organization

--- src/main/java/de/mpg/imeji/logic/vo/Person.java
+++ src/main/java/de/mpg/imeji/logic/vo/Person.java
@@ -108,8 +108,8 @@ public class Person
     {
         this.role = role;
     }
-    
-    @XmlElements(value=@XmlElement(name = "organizationalunit", namespace = "http://purl.org/escidoc/metadata/profiles/0.1"))
+
+    @XmlElements(value = @XmlElement(name = "organizationalunit", namespace = "http://purl.org/escidoc/metadata/profiles/0.1"))
     public Collection<Organization> getOrganizations()
     {
         return organizations;
@@ -165,6 +165,11 @@ public class Person
         return s;
     }
 
+    /**
+     * The full text to search for this person
+     * 
+     * @return
+     */
     public String AsFullText()
     {
         String str = givenName + " " + familyName + " " + alternativeName;
@@ -173,5 +178,23 @@ public class Person
             str += " " + org.getName();
         }
         return str.trim();
+    }
+
+    @Override
+    public Person clone()
+    {
+        Person clone = new Person();
+        clone.alternativeName = this.alternativeName;
+        clone.completeName = this.completeName;
+        clone.familyName = this.familyName;
+        clone.givenName = this.givenName;
+        clone.identifier = this.identifier;
+        for (Organization org : this.organizations)
+        {
+            clone.organizations.add(org.clone());
+        }
+        clone.role = this.role;
+        clone.pos = this.pos;
+        return clone;
     }
 }

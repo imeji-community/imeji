131316e4c092d8753797287fe5c1a90052bc5db8
Bastien Saquet <saquet@mpdl.mpg.de>
2013-07-29 19:17:18 +0200

    add missing files to previous commit

---
+++ src/main/java/de/mpg/imeji/logic/storage/transform/ImageGeneratorManager.java
@@ -0,0 +1,169 @@
+/*
+ *
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License"). You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at license/ESCIDOC.LICENSE
+ * or http://www.escidoc.de/license.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at license/ESCIDOC.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+/*
+ * Copyright 2006-2007 Fachinformationszentrum Karlsruhe Gesellschaft
+ * für wissenschaftlich-technische Information mbH and Max-Planck-
+ * Gesellschaft zur Förderung der Wissenschaft e.V.
+ * All rights reserved. Use is subject to license terms.
+ */
+package de.mpg.imeji.logic.storage.transform;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import de.mpg.imeji.logic.storage.Storage.FileResolution;
+import de.mpg.imeji.logic.storage.transform.impl.MagickImageGenerator;
+import de.mpg.imeji.logic.storage.transform.impl.MicroscopeImageGenerator;
+import de.mpg.imeji.logic.storage.transform.impl.PdfImageGenerator;
+import de.mpg.imeji.logic.storage.transform.impl.SimpleImageGenerator;
+import de.mpg.imeji.logic.storage.transform.impl.XuggleImageGenerator;
+import de.mpg.imeji.logic.storage.util.GifUtils;
+import de.mpg.imeji.logic.storage.util.ImageUtils;
+import de.mpg.imeji.logic.storage.util.StorageUtils;
+
+/**
+ * Implements all process to generate the images
+ * 
+ * @author saquet (initial creation)
+ * @author $Author$ (last modification)
+ * @version $Revision$ $LastChangedDate$
+ */
+public class ImageGeneratorManager
+{
+    public Iterator<ImageGenerator> generators = null;
+
+    /**
+     * Default constructor of {@link ImageGeneratorManager}
+     */
+    public ImageGeneratorManager()
+    {
+        List<ImageGenerator> l = new ArrayList<ImageGenerator>();
+        l.add(new PdfImageGenerator());
+        l.add(new XuggleImageGenerator());
+        l.add(new MagickImageGenerator());
+        l.add(new SimpleImageGenerator());
+        l.add(new MicroscopeImageGenerator());
+        generators = l.iterator();
+    }
+
+    /**
+     * Generate a Thumbnail image for imeji
+     * 
+     * @param bytes
+     * @param extension
+     * @return
+     */
+    public byte[] generateThumbnail(byte[] bytes, String extension)
+    {
+        return generate(bytes, extension, FileResolution.THUMBNAIL);
+    }
+
+    /**
+     * Generate a Web resolution image for imeji
+     * 
+     * @param bytes
+     * @param extension
+     * @return
+     */
+    public byte[] generateWebResolution(byte[] bytes, String extension)
+    {
+        return generate(bytes, extension, FileResolution.WEB);
+    }
+
+    /**
+     * Generate an image (only jpg and gif supported here) into a smaller image according to the {@link FileResolution}
+     * 
+     * @param bytes
+     * @param extension
+     * @param resolution
+     * @return
+     */
+    private byte[] generate(byte[] bytes, String extension, FileResolution resolution)
+    {
+        if (StorageUtils.compareExtension("gif", extension))
+        {
+            return generateGif(bytes, extension, resolution);
+        }
+        return generateJpeg(bytes, extension, resolution);
+    }
+
+    /**
+     * Generate an animated gif in the wished size. The file must be a gif, otherwise an exception is thrown
+     * 
+     * @param bytes
+     * @param extension
+     * @param resolution
+     * @return
+     */
+    private byte[] generateGif(byte[] bytes, String extension, FileResolution resolution)
+    {
+        try
+        {
+            return GifUtils.resizeAnimatedGif(bytes, resolution);
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException("Error transforming gif:", e);
+        }
+    }
+
+    /**
+     * Generate an jpeg image in the wished size.
+     * @param bytes
+     * @param extension
+     * @param resolution
+     * @return
+     */
+    private byte[] generateJpeg(byte[] bytes, String extension, FileResolution resolution)
+    {
+        try
+        {
+            return ImageUtils.transformImage(toJpeg(bytes, extension), resolution, StorageUtils.getMimeType("jpg"));
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException("Error transforming image:", e);
+        }
+    }
+
+    /**
+     * Uses the {@link ImageGenerator} to transform the bytes into a jpg
+     * 
+     * @param bytes
+     * @param extension
+     * @return
+     */
+    private byte[] toJpeg(byte[] bytes, String extension)
+    {
+        byte[] jpeg = null;
+        while (generators.hasNext() && jpeg == null)
+        {
+            ImageGenerator imageGenerator = (ImageGenerator)generators.next();
+            jpeg = imageGenerator.generateJPG(bytes, extension);
+        }
+        if (jpeg == null)
+            throw new RuntimeException("Unsupported file format (requested was " + extension + ")");
+        return jpeg;
+    }
+}
---
+++ src/main/java/de/mpg/imeji/logic/storage/transform/impl/PdfImageGenerator.java
@@ -0,0 +1,55 @@
+/*
+*
+* CDDL HEADER START
+*
+* The contents of this file are subject to the terms of the
+* Common Development and Distribution License, Version 1.0 only
+* (the "License"). You may not use this file except in compliance
+* with the License.
+*
+* You can obtain a copy of the license at license/ESCIDOC.LICENSE
+* or http://www.escidoc.de/license.
+* See the License for the specific language governing permissions
+* and limitations under the License.
+*
+* When distributing Covered Code, include this CDDL HEADER in each
+* file and include the License file at license/ESCIDOC.LICENSE.
+* If applicable, add the following below this CDDL HEADER, with the
+* fields enclosed by brackets "[]" replaced with your own identifying
+* information: Portions Copyright [yyyy] [name of copyright owner]
+*
+* CDDL HEADER END
+*/
+
+/*
+* Copyright 2006-2007 Fachinformationszentrum Karlsruhe Gesellschaft
+* für wissenschaftlich-technische Information mbH and Max-Planck-
+* Gesellschaft zur Förderung der Wissenschaft e.V.
+* All rights reserved. Use is subject to license terms.
+*/ 
+
+package de.mpg.imeji.logic.storage.transform.impl;
+
+import de.mpg.imeji.logic.storage.transform.ImageGenerator;
+
+/**
+ * TODO Description
+ *
+ * @author saquet (initial creation)
+ * @author $Author$ (last modification)
+ * @version $Revision$ $LastChangedDate$
+ *
+ */
+public class PdfImageGenerator implements ImageGenerator
+{
+
+    /* (non-Javadoc)
+     * @see de.mpg.imeji.logic.storage.transform.ImageGenerator#generateJPG(byte[], java.lang.String)
+     */
+    @Override
+    public byte[] generateJPG(byte[] bytes, String extension)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}

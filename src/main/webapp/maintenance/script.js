25fd0ce51bc67252a08e949edc4bbfe24c3434fc
Hai Nguyen <h.nguyen@fu-berlin.de>
2013-09-06 15:10:04 +0200

    - added maintenance webpage

---
+++ src/main/webapp/maintenance/script.js
@@ -0,0 +1,60 @@
+/* begin Page */
+// css helper
+(function($) {
+    var data = [
+        {str:navigator.userAgent,sub:'Chrome',ver:'Chrome',name:'chrome'},
+        {str:navigator.vendor,sub:'Apple',ver:'Version',name:'safari'},
+        {prop:window.opera,ver:'Opera',name:'opera'},
+        {str:navigator.userAgent,sub:'Firefox',ver:'Firefox',name:'firefox'},
+        {str:navigator.userAgent,sub:'MSIE',ver:'MSIE',name:'ie'}];
+    for (var n=0;n<data.length;n++)	{
+        if ((data[n].str && (data[n].str.indexOf(data[n].sub) != -1)) || data[n].prop) {
+            var v = function(s){var i=s.indexOf(data[n].ver);return (i!=-1)?parseInt(s.substring(i+data[n].ver.length+1)):'';};
+            $('html').addClass(data[n].name+' '+data[n].name+v(navigator.userAgent) || v(navigator.appVersion)); break;			
+        }
+    }
+})(jQuery);
+/* end Page */
+
+/* begin Layout */
+jQuery(function () {
+     var c = jQuery('div.art-content');
+    if (c.length !== 1) return;
+    var s = c.parent().children('.art-layout-cell:not(.art-content)');
+
+
+    if (jQuery.browser.msie && parseInt(jQuery.browser.version) < 8) {
+        jQuery(window).bind('resize', function() {
+            var w = 0;
+            c.hide();
+            s.each(function() { w += this.clientWidth; });
+            c.w = c.parent().width(); c.css('width', c.w - w + 'px');
+            c.show();
+        });
+    }
+
+    jQuery(window).trigger('resize');
+});/* end Layout */
+
+/* begin Button */
+function artButtonSetup(className) {
+    jQuery.each(jQuery("a." + className + ", button." + className + ", input." + className), function (i, val) {
+        var b = jQuery(val);
+        if (!b.parent().hasClass('art-button-wrapper')) {
+            if (b.is('input')) b.val(b.val().replace(/^\s*/, '')).css('zoom', '1');
+            if (!b.hasClass('art-button')) b.addClass('art-button');
+            jQuery("<span class='art-button-wrapper'><span class='art-button-l'> </span><span class='art-button-r'> </span></span>").insertBefore(b).append(b);
+            if (b.hasClass('active')) b.parent().addClass('active');
+        }
+        b.mouseover(function () { jQuery(this).parent().addClass("hover"); });
+        b.mouseout(function () { var b = jQuery(this); b.parent().removeClass("hover"); if (!b.hasClass('active')) b.parent().removeClass('active'); });
+        b.mousedown(function () { var b = jQuery(this); b.parent().removeClass("hover"); if (!b.hasClass('active')) b.parent().addClass('active'); });
+        b.mouseup(function () { var b = jQuery(this); if (!b.hasClass('active')) b.parent().removeClass('active'); });
+    });
+}
+jQuery(function() { artButtonSetup("art-button"); });
+
+/* end Button */
+
+
+
--- src/main/webapp/resources/digilib/jquery/jquery.digilib.js
+++ src/main/webapp/resources/digilib/jquery/jquery.digilib.js
@@ -48,11 +48,11 @@ if (typeof console === 'undefined') {
         // homepage url (behind logo)
         'homeUrl' : '/',
         // base URL to digilib webapp (e.g. 'http://digilib.mpiwg-berlin.mpg.de/digitallibrary')
-        'digilibBaseUrl' : '/imeji/resources/digilib',
+        'digilibBaseUrl' : '/resources/digilib',
         // path to digilib frontend page (inside digilibBaseUrl)
-        'digilibFrontendPath' : '/imeji/digilibImage',
+        'digilibFrontendPath' : '/digilibImage',
         // base URL to Scaler servlet (usually digilibBaseUrl+'/servlet/Scaler')
-        'scalerBaseUrl' : '/imeji/digilib',
+        'scalerBaseUrl' : '/digilib',
         // prefix of digilib classes in CSS styles
         'cssPrefix' : 'dl-',
         // list of Scaler parameters

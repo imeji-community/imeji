f1c907163787bd7e50f779bbb5b9a6f5feb71d34
Friederike Kleinfercher <kleinfercher@mpdl.mpg.de>
2014-02-11 13:50:07 +0100

    https://github.com/imeji-community/imeji/issues/413

--- src/main/java/de/mpg/imeji/presentation/image/ImagesBean.java
+++ src/main/java/de/mpg/imeji/presentation/image/ImagesBean.java
@@ -155,7 +155,7 @@ public class ImagesBean extends BasePaginatorListSessionBean<ThumbnailBean>
         sortMenu = new ArrayList<SelectItem>();
         sortMenu.add(new SelectItem(null, "--"));
         sortMenu.add(new SelectItem(SearchIndex.names.created, session.getLabel("sort_img_date_created")));
-        sortMenu.add(new SelectItem(SearchIndex.names.modified, session.getLabel("sort_img_date_mod")));
+        sortMenu.add(new SelectItem(SearchIndex.names.modified, session.getLabel("sort_date_mod")));
         sortMenu.add(new SelectItem(SearchIndex.names.cont_title, session.getLabel("sort_img_collection")));
         sortMenu.add(new SelectItem(SearchIndex.names.filename, session.getLabel("sort_img_filename")));
     }

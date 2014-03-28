a232f3e4170e42c29903ff27e274d16ddf8df322
Bastien Saquet <saquet@mpdl.mpg.de>
2014-03-28 11:42:41 +0100

    remove sysout

--- src/main/java/de/mpg/imeji/logic/search/Search.java
+++ src/main/java/de/mpg/imeji/logic/search/Search.java
@@ -116,7 +116,6 @@ public class Search
     public List<String> searchSimpleForQuery(String sparqlQuery, SortCriterion sortCri)
     {
         SearchResult sr = new SearchResult(ImejiSPARQL.exec(sparqlQuery, getModelName(type)), sortCri);
-        System.out.println(sparqlQuery);
         return sr.getResults();
     }
 

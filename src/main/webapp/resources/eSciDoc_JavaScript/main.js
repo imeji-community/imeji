c2f09f226a14b52cee624e9343413da11d81093e
Bastien Saquet <saquet@mpdl.mpg.de>
2014-04-02 15:00:32 +0200

    profile edit: reference to js method changed

--- src/main/webapp/resources/eSciDoc_JavaScript/main.js
+++ src/main/webapp/resources/eSciDoc_JavaScript/main.js
@@ -2,13 +2,13 @@
  * Parse the id define in the css class by id_class
  * 
  * @param classname
- * [@param classRef]
+ *            [@param classRef]
  * @returns
  */
 function parseId(classname, classRef) {
 	var ptn;
 	if (classRef) {
-		ptn = new RegExp(classRef+'(\\S*)');
+		ptn = new RegExp(classRef + '(\\S*)');
 	} else {
 		ptn = /id_(\S*)/;
 	}
@@ -16,9 +16,9 @@ function parseId(classname, classRef) {
 	if (RegExp.$1 != "") {
 		return RegExp.$1;
 	} else {
-		 return false;
+		return false;
 	}
-}
+};
 
 /**
  * JQuery event for Highlight methods
@@ -28,28 +28,32 @@ function highlighter() {
 		jQuery(".highlight_area").mouseover(function() {
 			var itemId = parseId(jQuery(this).attr('class'));
 			var prntId = parseId(jQuery(this).attr('class'), 'parent_');
-			highlight(itemId);	// highlight the current element
-			checkForChilds(itemId);// highlight the child elements - recursive - of the current item, if it's given
-			if (prntId) { //check if the current item is a child of another
-				highlight(prntId, 'id_'); // hightlight the next parent item, if the current item is a child
+			highlight(itemId); // highlight the current element
+			checkForChilds(itemId);// highlight the child elements - recursive
+									// - of the current item, if it's given
+			if (prntId) { // check if the current item is a child of another
+				highlight(prntId, 'id_'); // hightlight the next parent item,
+											// if the current item is a child
 			}
 		}).mouseout(function() {
 			reset_highlight();
 		});
 	});
-}
+};
 /**
- * check if items given with the parent id.
- * It's triggered through mouse-over event in highlighter function.
+ * check if items given with the parent id. It's triggered through mouse-over
+ * event in highlighter function.
+ * 
  * @param id
  */
 function checkForChilds(id) {
 	var childs, curId;
-	//all childs have a parent-class with the parent id
-	childs = jQuery('.parent_'+id);
-	
-	if (childs.length > 0) {	// if childs given
-		childs.each(function(i, obj){	// loop through the childs an check if themselves have also childs
+	// all childs have a parent-class with the parent id
+	childs = jQuery('.parent_' + id);
+
+	if (childs.length > 0) { // if childs given
+		childs.each(function(i, obj) { // loop through the childs an check if
+										// themselves have also childs
 			jQuery(this).addClass('imj_highlightDependencies');
 			curId = parseId(jQuery(this).attr('class'));
 			if (curId) {
@@ -57,7 +61,7 @@ function checkForChilds(id) {
 			}
 		});
 	}
-}
+};
 
 /**
  * Highlight the element with id passed in the parameter. If it has children
@@ -65,24 +69,21 @@ function checkForChilds(id) {
  * is then recognized by the css class "id_ +id"
  * 
  * @param id
- * [@param alter]
+ *            [@param alter]
  */
 function highlight(id, alter) {
 	var items;
-	items = (alter) ? jQuery('.'+alter+id) : jQuery('.id_' + id);
+	items = (alter) ? jQuery('.' + alter + id) : jQuery('.id_' + id);
 	items.addClass("imj_highlightDependencies");
-}
+};
 /**
  * Reset highlighted element to their original value. Should be triggered on
- * mouse out
- * DELETE FUNCTION HIGHLIGHT + DEPENDENCIES and create/use css definitions
+ * mouse out DELETE FUNCTION HIGHLIGHT + DEPENDENCIES and create/use css
+ * definitions
  */
 function reset_highlight() {
-	$('.imj_highlightDependencies').removeClass("imj_highlightDependencies");
-}
-
-
-
+	jQuery('.imj_highlightDependencies').removeClass("imj_highlightDependencies");
+};
 
 /**
  * Trigger the highlight on page load
@@ -91,7 +92,6 @@ jQuery(document).ready(function() {
 	highlighter();
 });
 
-
 /*******************************************************************************
  * START : Function for the Metadata Profile pages:
  ******************************************************************************/
@@ -99,42 +99,43 @@ jQuery(document).ready(function() {
  * JQUERY UI Drag and drop for metadata
  */
 function dragAndDrop() {
-	$(function() {
+	jQuery(function() {
 		var dragged;
-		$(".draggable").draggable({
+		jQuery(".draggable").draggable({
 			zIndex : 100,
 			opacity : 0.1,
 			cursor : "move",
 			axis : "y",
 			revert : 'invalid',
 		});
-		$(".draggable").on("dragstart", function(event, ui) {
-			dragged = $(this).attr('id') + 'DragButton';
-			hide_non_droppable_area(parseId($(this).attr('class')));
+		jQuery(".draggable").on("dragstart", function(event, ui) {
+			dragged = jQuery(this).attr('id') + 'DragButton';
+			hide_non_droppable_area(parseId(jQuery(this).attr('class')));
 		});
-		$(".draggable").on("dragstop", function(event, ui) {
+		jQuery(".draggable").on("dragstop", function(event, ui) {
 			show_all_droppable_elements();
 		});
-		$(".dropMetadata").droppable({
+		jQuery(".dropMetadata").droppable({
 			hoverClass : "drop-hover",
 			accept : '.draggable',
 			activeClass : "dropMetadataActive",
 			greedy : true,
 			drop : function(event, ui) {
 				setOpacity();
-				var id = $(this).attr('id') + "Button";
+				var id = jQuery(this).attr('id') + "Button";
 				document.getElementById(dragged).click();
 				document.getElementById(id).click();
 			}
 		});
-		$(".dropChild").droppable(
+		jQuery(".dropChild").droppable(
 				{
 					hoverClass : "drop-hover",
 					accept : '.draggable',
 					activeClass : "dropChildActive",
 					drop : function(event, ui) {
 						setOpacity();
-						var id = $(this).attr('id').replace("metadata", "dropChildButton");
+						var id = jQuery(this).attr('id').replace("metadata",
+								"dropChildButton");
 						document.getElementById(dragged).click();
 						document.getElementById(id).click();
 					}
@@ -239,7 +240,7 @@ function hide_childs(id) {
  * @param button
  * @param message
  */
-//seems to be unused - March 31th, 2014
+// seems to be unused - March 31th, 2014
 function submitPanel(panelId, message) {
 	var panel = document.getElementById(panelId);
 	if (panel != null) {
@@ -253,43 +254,46 @@ function submitPanel(panelId, message) {
  */
 var currentViewState;
 if (typeof jsf !== 'undefined') {
-	jsf.ajax.addOnEvent(function(e) {
-		var xml = e.responseXML;
-		var source = e.source;
-		var status = e.status;
-		if (status === 'success') {
-			var response = xml.getElementsByTagName('partial-response')[0];
-			if (response !== null) {
-				var changes = response.getElementsByTagName('changes')[0];
-				if (changes != undefined) {
-					var updates = changes.getElementsByTagName('update');
-					if (updates != undefined) {
-						for ( var i = 0; i < updates.length; i++) {
-							var update = updates[i];
-							var id = update.getAttribute('id');
-							if (id === 'javax.faces.ViewState') {
-								currentViewState = update.firstChild.data;
-								// update all forms
-								var forms = document.forms;
-								for ( var j = 0; j < forms.length; j++) {
-									var form = forms[j];
-									var field = form.elements["javax.faces.ViewState"];
-									if (typeof field == 'undefined') {
-										field = document.createElement("input");
-										field.type = "hidden";
-										field.name = "javax.faces.ViewState";
-										form.appendChild(field);
+	jsf.ajax
+			.addOnEvent(function(e) {
+				var xml = e.responseXML;
+				var source = e.source;
+				var status = e.status;
+				if (status === 'success') {
+					var response = xml.getElementsByTagName('partial-response')[0];
+					if (response !== null) {
+						var changes = response.getElementsByTagName('changes')[0];
+						if (changes != undefined) {
+							var updates = changes
+									.getElementsByTagName('update');
+							if (updates != undefined) {
+								for ( var i = 0; i < updates.length; i++) {
+									var update = updates[i];
+									var id = update.getAttribute('id');
+									if (id === 'javax.faces.ViewState') {
+										currentViewState = update.firstChild.data;
+										// update all forms
+										var forms = document.forms;
+										for ( var j = 0; j < forms.length; j++) {
+											var form = forms[j];
+											var field = form.elements["javax.faces.ViewState"];
+											if (typeof field == 'undefined') {
+												field = document
+														.createElement("input");
+												field.type = "hidden";
+												field.name = "javax.faces.ViewState";
+												form.appendChild(field);
+											}
+											field.value = currentViewState;
+										}
 									}
-									field.value = currentViewState;
 								}
 							}
 						}
 					}
 				}
-			}
-		}
 
-	});
+			});
 }
 
 /**
@@ -297,62 +301,82 @@ if (typeof jsf !== 'undefined') {
  */
 var patchJSF = function() {
 	if (typeof jsf !== 'undefined') {
-		jsf.ajax.addOnEvent(function(e) {
-			if (e.status === 'success') {
-				$("partial-response:first changes:first update[id='javax.faces.ViewState']",
-						e.responseXML).each(function(i, u) {
-							// update all forms
-							$(document.forms).each(
-								function(i, f) {
-									var field = $("input[name='javax.faces.ViewState']", f);
-									if (field.length == 0) {
-										field = $("<input type=\"hidden\" name=\"javax.faces.ViewState\" />").appendTo(f);
-									}
-									field.val(u.firstChild.data);
-								});
-						});
-			}
-		});
+		jsf.ajax
+				.addOnEvent(function(e) {
+					if (e.status === 'success') {
+						$(
+								"partial-response:first changes:first update[id='javax.faces.ViewState']",
+								e.responseXML)
+								.each(
+										function(i, u) {
+											// update all forms
+											$(document.forms)
+													.each(
+															function(i, f) {
+																var field = $(
+																		"input[name='javax.faces.ViewState']",
+																		f);
+																if (field.length == 0) {
+																	field = $(
+																			"<input type=\"hidden\" name=\"javax.faces.ViewState\" />")
+																			.appendTo(
+																					f);
+																}
+																field
+																		.val(u.firstChild.data);
+															});
+										});
+					}
+				});
 	}
 };
 
-/* open a dialog
- * functions are shifted and modified from old template.xhtml
+/*
+ * open a dialog functions are shifted and modified from old template.xhtml
  */
 function openDialog(id) {
-	/*set the dialog in center of the screen*/
+	/* set the dialog in center of the screen */
 	var dialog = $(document.getElementById(id));
-	dialog.css("left", Math.max(0, Math.round(($(window).width() - $(dialog).outerWidth()) / 2) + $(window).scrollLeft()) + "px");
-	/*open the dialog*/
+	dialog.css("left", Math.max(0, Math.round(($(window).width() - $(dialog)
+			.outerWidth()) / 2)
+			+ $(window).scrollLeft())
+			+ "px");
+	/* open the dialog */
 	dialog.show();
 	$(".imj_modalDialogBackground").show();
 }
-/*close a dialog*/
+/* close a dialog */
 function closeDialog(id) {
 	var dialog = $(document.getElementById(id));
 	$(".imj_modalDialogBackground").hide();
 	dialog.hide();
 }
 
-$(window).resize(function(evt){
-	var dialog = $('.imj_modalDialogBox:visible');
-	if (dialog.length > 0) {
-		dialog.css("left", Math.max(0, Math.round(($(window).width() - $(dialog).outerWidth()) / 2) + $(window).scrollLeft()) + "px");
-	}
-});
+$(window).resize(
+		function(evt) {
+			var dialog = $('.imj_modalDialogBox:visible');
+			if (dialog.length > 0) {
+				dialog.css("left", Math.max(0,
+						Math
+								.round(($(window).width() - $(dialog)
+										.outerWidth()) / 2)
+								+ $(window).scrollLeft())
+						+ "px");
+			}
+		});
 
 /*
- * Extended usability function to set the content width of
- * overlay menu to the minimum of the trigger width.
- * It will be called one time after page loading is finished
+ * Extended usability function to set the content width of overlay menu to the
+ * minimum of the trigger width. It will be called one time after page loading
+ * is finished
  */
-$(function(){
-	$('.imj_overlayMenu').each(function(i, obj){
+$(function() {
+	$('.imj_overlayMenu').each(function(i, obj) {
 		var menuHeaderWidth = $(this).find(".imj_menuHeader").width();
 		var menuBody = $(this).find(".imj_menuBody");
 		if (menuHeaderWidth > menuBody.width()) {
 			menuBody.width(menuHeaderWidth);
 		}
-		
+
 	});
 });

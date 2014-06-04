7ad988918f5ddb34d00c92bc52b6074d3656b7de
Bastien Saquet <saquet@mpdl.mpg.de>
2014-06-04 13:41:42 +0200

    https://github.com/imeji-community/imeji/issues/608

--- src/main/java/de/mpg/imeji/logic/util/DateFormatter.java
+++ src/main/java/de/mpg/imeji/logic/util/DateFormatter.java
@@ -3,7 +3,6 @@
  */
 package de.mpg.imeji.logic.util;
 
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
--- src/main/java/de/mpg/imeji/presentation/util/CacheControlPhaseListener.java
+++ src/main/java/de/mpg/imeji/presentation/util/CacheControlPhaseListener.java
@@ -5,28 +5,29 @@ import javax.faces.event.PhaseEvent;
 import javax.faces.event.PhaseId;
 import javax.faces.event.PhaseListener;
 import javax.servlet.http.HttpServletResponse;
- 
+
 public class CacheControlPhaseListener implements PhaseListener
 {
+    private static final long serialVersionUID = 4651265325754141047L;
+
     public PhaseId getPhaseId()
     {
         return PhaseId.RENDER_RESPONSE;
     }
- 
+
     public void afterPhase(PhaseEvent event)
     {
     }
- 
-    public void beforePhase(PhaseEvent event)
-    { 
-        FacesContext facesContext = event.getFacesContext();
-        HttpServletResponse response = (HttpServletResponse) facesContext.getExternalContext().getResponse();
 
-     // Set standard HTTP/1.1 no-cache headers.
+    public void beforePhase(PhaseEvent event)
+    {
+        FacesContext facesContext = event.getFacesContext();
+        HttpServletResponse response = (HttpServletResponse)facesContext.getExternalContext().getResponse();
+        // Set standard HTTP/1.1 no-cache headers.
         response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
         // Set standard HTTP/1.0 no-cache header.
         response.setHeader("Pragma", "no-cache");
-        //Proxies
+        // Proxies
         response.setDateHeader("Expires", 0);
     }
 }

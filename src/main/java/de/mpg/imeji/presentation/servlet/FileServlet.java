65d3971166e3f280eb9e28535ad3eaa61cf67111
Bastien Saquet <saquet@mpdl.mpg.de>
2014-11-06 17:12:28 +0100

    File Servlet: force http authentication before session

--- src/main/java/de/mpg/imeji/presentation/servlet/FileServlet.java
+++ src/main/java/de/mpg/imeji/presentation/servlet/FileServlet.java
@@ -18,6 +18,7 @@ import org.apache.log4j.Logger;
 
 import de.mpg.imeji.logic.Imeji;
 import de.mpg.imeji.logic.ImejiSPARQL;
+import de.mpg.imeji.logic.auth.AuthenticationFactory;
 import de.mpg.imeji.logic.auth.Authorization;
 import de.mpg.imeji.logic.controller.CollectionController;
 import de.mpg.imeji.logic.controller.ItemController;
@@ -45,214 +46,204 @@ import de.mpg.imeji.presentation.util.PropertyReader;
  * @author $Author$ (last modification)
  * @version $Revision$ $LastChangedDate$
  */
-public class FileServlet extends HttpServlet
-{
-    private static final long serialVersionUID = 5502546330318540997L;
-    private static Logger logger = Logger.getLogger(FileServlet.class);
-    private StorageController storageController;
-    private Authorization authorization;
-    private Navigation navivation;
-    private String domain;
-    private String digilibUrl;
-    /**
-     * If property imeji.storage.path= /data/imeji/files/ then, internalStorageRoot = files
-     */
-    private String internalStorageRoot;
-    /**
-     * The path for this servlet as defined in the web.xml
-     */
-    public static final String SERVLET_PATH = "file";
+public class FileServlet extends HttpServlet {
+	private static final long serialVersionUID = 5502546330318540997L;
+	private static Logger logger = Logger.getLogger(FileServlet.class);
+	private StorageController storageController;
+	private Authorization authorization;
+	private Navigation navivation;
+	private String domain;
+	private String digilibUrl;
+	/**
+	 * If property imeji.storage.path= /data/imeji/files/ then,
+	 * internalStorageRoot = files
+	 */
+	private String internalStorageRoot;
+	/**
+	 * The path for this servlet as defined in the web.xml
+	 */
+	public static final String SERVLET_PATH = "file";
 
-    @Override
-    public void init()
-    {
-        try
-        {
-            storageController = new StorageController();
-            logger.info("File Servlet initialized");
-            authorization = new Authorization();
-            navivation = new Navigation();
-            domain = StringHelper.normalizeURI(navivation.getDomain());
-            domain = domain.substring(0, domain.length() - 1);
-            digilibUrl = PropertyReader.getProperty("digilib.imeji.instance.url");
-            if (digilibUrl != null && !digilibUrl.isEmpty())
-                digilibUrl = StringHelper.normalizeURI(digilibUrl);
-            InternalStorageManager ism = new InternalStorageManager();
-            internalStorageRoot = FilenameUtils
-                    .getBaseName(FilenameUtils.normalizeNoEndSeparator(ism.getStoragePath()));
-        }
-        catch (Exception e)
-        {
-            throw new RuntimeException("Image servlet not initialized! " + e);
-        }
-    }
+	@Override
+	public void init() {
+		try {
+			storageController = new StorageController();
+			logger.info("File Servlet initialized");
+			authorization = new Authorization();
+			navivation = new Navigation();
+			domain = StringHelper.normalizeURI(navivation.getDomain());
+			domain = domain.substring(0, domain.length() - 1);
+			digilibUrl = PropertyReader
+					.getProperty("digilib.imeji.instance.url");
+			if (digilibUrl != null && !digilibUrl.isEmpty())
+				digilibUrl = StringHelper.normalizeURI(digilibUrl);
+			InternalStorageManager ism = new InternalStorageManager();
+			internalStorageRoot = FilenameUtils.getBaseName(FilenameUtils
+					.normalizeNoEndSeparator(ism.getStoragePath()));
+		} catch (Exception e) {
+			throw new RuntimeException("Image servlet not initialized! " + e);
+		}
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
-    {
-        String url = req.getParameter("id");
-        boolean download = "1".equals(req.getParameter("download"));
-        if (url == null)
-        {
-            // if the id parameter is null, interpret the whole url as a direct to the file (can only work if the
-            // internal storage is used)
-            url = domain + req.getRequestURI();
-        }
-        resp.setContentType(StorageUtils.getMimeType(StringHelper.getFileExtension(url)));
-        SessionBean session = getSession(req);
-        if (authorization.read(getUser(session), loadCollection(url, session))
-                || authorization.read(getUser(session), getItem(url, getUser(session))))
-        {
-            if (download)
-                resp.setHeader("Content-disposition", "attachment;");
-            storageController.read(url, resp.getOutputStream(), true);
-        }
-        else
-        {
-            resp.sendError(403, "imeji security: You are not allowed to view this file");
-        }
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String url = req.getParameter("id");
+		boolean download = "1".equals(req.getParameter("download"));
+		if (url == null) {
+			// if the id parameter is null, interpret the whole url as a direct
+			// to the file (can only work if the
+			// internal storage is used)
+			url = domain + req.getRequestURI();
+		}
+		resp.setContentType(StorageUtils.getMimeType(StringHelper
+				.getFileExtension(url)));
+		SessionBean session = getSession(req);
+		User user = getUser(req, session);
+		
+		if (authorization.read(user, loadCollection(url, session))
+				|| authorization.read(user, getItem(url, user))) {
+			if (download)
+				resp.setHeader("Content-disposition", "attachment;");
+			storageController.read(url, resp.getOutputStream(), true);
+		} else {
+			resp.sendError(403,
+					"imeji security: You are not allowed to view this file");
+		}
+	}
 
-    /**
-     * Load a {@link CollectionImeji} from the session if possible, otherwise from jena
-     * 
-     * @param uri
-     * @param user
-     * @return
-     * @throws Exception
-     */
-    private CollectionImeji loadCollection(String url, SessionBean session)
-    {
-        if (session == null)
-            return loadCollection(url);
-        URI collectionURI = getCollectionURI(url);
-        if (collectionURI == null)
-            return null;
-        CollectionImeji collection = session.getCollectionCached().get(collectionURI);
-        if (collection == null)
-        {
-            try
-            {
-                // important to use lazy load, otherwise high performance issue
-                collection = ObjectLoader.loadCollectionLazy(collectionURI, Imeji.adminUser);
-                session.getCollectionCached().put(collection.getId(), collection);
-            }
-            catch (Exception e)
-            {
-                /* user is not allowed to view this collection */
-            }
-        }
-        return collection;
-    }
+	/**
+	 * Load a {@link CollectionImeji} from the session if possible, otherwise
+	 * from jena
+	 * 
+	 * @param uri
+	 * @param user
+	 * @return
+	 * @throws Exception
+	 */
+	private CollectionImeji loadCollection(String url, SessionBean session) {
+		if (session == null)
+			return loadCollection(url);
+		URI collectionURI = getCollectionURI(url);
+		if (collectionURI == null)
+			return null;
+		CollectionImeji collection = session.getCollectionCached().get(
+				collectionURI);
+		if (collection == null) {
+			try {
+				// important to use lazy load, otherwise high performance issue
+				collection = ObjectLoader.loadCollectionLazy(collectionURI,
+						Imeji.adminUser);
+				session.getCollectionCached().put(collection.getId(),
+						collection);
+			} catch (Exception e) {
+				/* user is not allowed to view this collection */
+			}
+		}
+		return collection;
+	}
 
-    /**
-     * Load a {@link CollectionImeji} when the session is null
-     * 
-     * @param url
-     * @return
-     */
-    private CollectionImeji loadCollection(String url)
-    {
-        List<String> l = ImejiSPARQL.exec(SPARQLQueries.selectCollectionIdOfFile(url), null);
-        if (l.size() == 0)
-            throw new RuntimeException("File " + url + " couldn't be found");
-        CollectionController c = new CollectionController();
-        try
-        {
-            return c.retrieve(URI.create(l.get(0)), null);
-        }
-        catch (Exception e)
-        {
-            logger.error(e);
-        }
-        return null;
-    }
+	/**
+	 * Return the {@link User} of the request. Check first is a user is send
+	 * with the request. If not, check in the the session.
+	 * 
+	 * @param req
+	 * @return
+	 */
+	private User getUser(HttpServletRequest req, SessionBean session) {
+		User user = AuthenticationFactory.factory(req).doLogin();
+		if (user != null)
+			return user;
+		if (session != null) {
+			return session.getUser();
+		}
+		return null;
 
-    /**
-     * Return the uri of the {@link CollectionImeji} of the file with this url
-     * 
-     * @param url
-     * @return
-     */
-    private URI getCollectionURI(String url)
-    {
-        String id = storageController.getCollectionId(url);
-        if (id != null)
-        {
-            return ObjectHelper.getURI(CollectionImeji.class, id);
-        }
-        else
-        {
-            Search s = SearchFactory.create();
-            List<String> r = s.searchSimpleForQuery(SPARQLQueries.selectCollectionIdOfFile(url)).getResults();
-            if (!r.isEmpty())
-                return URI.create(r.get(0));
-            else
-                return null;
-        }
-    }
+	}
 
-    /**
-     * Find the {@link Item} which is owner of the file
-     * 
-     * @param url
-     * @return
-     */
-    private Item getItem(String url, User user)
-    {
-        Search s = SearchFactory.create();
-        List<String> r = s.searchSimpleForQuery(SPARQLQueries.selectItemIdOfFile(url)).getResults();
-        if (!r.isEmpty())
-        {
-            ItemController c = new ItemController();
-            try
-            {
-                return c.retrieve(URI.create(r.get(0)), user);
-            }
-            catch (Exception e)
-            {
-                return null;
-            }
-        }
-        return null;
-    }
+	/**
+	 * Load a {@link CollectionImeji} when the session is null
+	 * 
+	 * @param url
+	 * @return
+	 */
+	private CollectionImeji loadCollection(String url) {
+		List<String> l = ImejiSPARQL.exec(
+				SPARQLQueries.selectCollectionIdOfFile(url), null);
+		if (l.size() == 0)
+			throw new RuntimeException("File " + url + " couldn't be found");
+		CollectionController c = new CollectionController();
+		try {
+			return c.retrieve(URI.create(l.get(0)), null);
+		} catch (Exception e) {
+			logger.error(e);
+		}
+		return null;
+	}
 
-    /**
-     * Read the user in the session
-     * 
-     * @param req
-     * @return
-     */
-    private User getUser(SessionBean sessionBean)
-    {
-        if (sessionBean != null)
-        {
-            return sessionBean.getUser();
-        }
-        return null;
-    }
+	/**
+	 * Return the uri of the {@link CollectionImeji} of the file with this url
+	 * 
+	 * @param url
+	 * @return
+	 */
+	private URI getCollectionURI(String url) {
+		String id = storageController.getCollectionId(url);
+		if (id != null) {
+			return ObjectHelper.getURI(CollectionImeji.class, id);
+		} else {
+			Search s = SearchFactory.create();
+			List<String> r = s.searchSimpleForQuery(
+					SPARQLQueries.selectCollectionIdOfFile(url)).getResults();
+			if (!r.isEmpty())
+				return URI.create(r.get(0));
+			else
+				return null;
+		}
+	}
 
-    /**
-     * Return the {@link SessionBean} form the {@link HttpSession}
-     * 
-     * @param req
-     * @return
-     */
-    private SessionBean getSession(HttpServletRequest req)
-    {
-        return (SessionBean)req.getSession(true).getAttribute(SessionBean.class.getSimpleName());
-    }
+	/**
+	 * Find the {@link Item} which is owner of the file
+	 * 
+	 * @param url
+	 * @return
+	 */
+	private Item getItem(String url, User user) {
+		Search s = SearchFactory.create();
+		List<String> r = s.searchSimpleForQuery(
+				SPARQLQueries.selectItemIdOfFile(url)).getResults();
+		if (!r.isEmpty()) {
+			ItemController c = new ItemController();
+			try {
+				return c.retrieve(URI.create(r.get(0)), user);
+			} catch (Exception e) {
+				return null;
+			}
+		}
+		return null;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
-    {
-        // No post action
-        return;
-    }
+	/**
+	 * Return the {@link SessionBean} form the {@link HttpSession}
+	 * 
+	 * @param req
+	 * @return
+	 */
+	private SessionBean getSession(HttpServletRequest req) {
+		return (SessionBean) req.getSession(true).getAttribute(
+				SessionBean.class.getSimpleName());
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// No post action
+		return;
+	}
 }

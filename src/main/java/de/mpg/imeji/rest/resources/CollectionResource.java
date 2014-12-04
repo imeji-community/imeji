22c3ecc2b4635127ca2ea86b0e6162d4d6bb485d
Bastien Saquet <saquet@mpdl.mpg.de>
2014-12-04 11:19:13 +0100

    Add comment to create collection

--- src/main/java/de/mpg/imeji/rest/resources/CollectionResource.java
+++ src/main/java/de/mpg/imeji/rest/resources/CollectionResource.java
@@ -29,47 +29,50 @@ import org.slf4j.LoggerFactory;
 //@Singleton
 @Path("/collections")
 @Api(value = "rest/collections", description = "Operations on collections")
-public class CollectionResource implements ImejiResource{
+public class CollectionResource implements ImejiResource {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CollectionResource.class);
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(CollectionResource.class);
 
-
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)  
-    public Response readAll(@Context HttpServletRequest req){       
-        return null;  
-    } 
-    
-    @GET   
-    @Path("/{id}")
-    @ApiOperation(value = "Get collection by id")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response readFromID(@Context HttpServletRequest req, @PathParam("id") String id){
-    	JSONResponse resp = CollectionProcess.readCollection(req, id);
-        return RestProcessUtils.buildJSONResponse(resp);
-    }  
-   
-    @POST 
-    @Consumes(MediaType.APPLICATION_JSON)
-    @ApiOperation(value = "Create collection or new version of collection", notes = "When contains as parameter \"versionOF\", new collection is created as a copy of a released collection (method needs to validate the status of the collection provided with \"versionOf\")\n"
-    + "\nCreation of the new collection automatically generates a new (empty) metadata profile  for that collection. ")
-    @Produces(MediaType.APPLICATION_JSON) 
-	public Response create(@Context HttpServletRequest req) {
-    	JSONResponse resp = CollectionProcess.createCollection(req);
-        return RestProcessUtils.buildJSONResponse(resp);
-	}  
-  
-    @DELETE  
-    @Path("/{id}")
-    @ApiOperation(value = "(Not implemented)  Delete collection by id", notes = "Deletes also the profile and items of this collection")   
-    @Produces(MediaType.APPLICATION_JSON) 
-	public Response delete(@Context HttpServletRequest req, @PathParam("id") String id) {
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response readAll(@Context HttpServletRequest req) {
 		return null;
 	}
 
+	@GET
+	@Path("/{id}")
+	@ApiOperation(value = "Get collection by id")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response readFromID(@Context HttpServletRequest req,
+			@PathParam("id") String id) {
+		JSONResponse resp = CollectionProcess.readCollection(req, id);
+		return RestProcessUtils.buildJSONResponse(resp);
+	}
 
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "Create collection or new version of collection", notes = "When contains as parameter \"versionOF\", new collection is created as a copy of a released collection (method needs to validate the status of the collection provided with \"versionOf\")\n"
+			+ "\nCreation of the new collection automatically generates a new (empty) metadata profile  for that collection. ")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response create(@Context HttpServletRequest req, String json) {
+		JSONResponse resp = CollectionProcess.createCollection(req);
+		return RestProcessUtils.buildJSONResponse(resp);
+	}
 
-    
+	@DELETE
+	@Path("/{id}")
+	@ApiOperation(value = "(Not implemented)  Delete collection by id", notes = "Deletes also the profile and items of this collection")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response delete(@Context HttpServletRequest req,
+			@PathParam("id") String id) {
+		return null;
+	}
 
+	@Override
+	public Response create(HttpServletRequest req) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }

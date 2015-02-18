5ae9b4b11972fcf0a8112ee07742a3b759e172e6
cafefish <yu@mpdl.mpg.de>
2015-02-19 00:21:38 +0100

    DIG-46, -47, -48

--- src/main/java/de/mpg/imeji/presentation/collection/CreateCollectionBean.java
+++ src/main/java/de/mpg/imeji/presentation/collection/CreateCollectionBean.java
@@ -3,6 +3,8 @@
  */
 package de.mpg.imeji.presentation.collection;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,13 +13,12 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
 
+import de.mpg.imeji.exceptions.ImejiException;
 import de.mpg.imeji.logic.controller.CollectionController;
-
+import de.mpg.imeji.logic.controller.ProfileController;
 import de.mpg.imeji.logic.vo.CollectionImeji;
-
 import de.mpg.imeji.logic.vo.Organization;
 import de.mpg.imeji.logic.vo.Person;
-
 import de.mpg.imeji.presentation.util.BeanHelper;
 import de.mpg.imeji.presentation.util.ImejiFactory;
 
@@ -114,30 +115,9 @@ public class CreateCollectionBean extends CollectionBean {
 	 * @throws Exception
 	 */
 	public String save() throws Exception {
-		if (valid()) {
-			// Create collection
-			CollectionController collectionController = new CollectionController();
-	        int pos = 0;
-	        // Set the position of the statement (used for the sorting later)
-	        for (Person p : getCollection().getMetadata().getPersons())
-	        {
-	            p.setPos(pos);
-	            pos++;
-	            int pos2 = 0;
-	            for(Organization o : p.getOrganizations()){
-	            	o.setPos(pos2);
-	            	pos2++;	            	
-	            }
-	        }  
-			collectionController.create(getCollection(), null, sessionBean.getUser());
-			if(isUseMDProfileTemplate()){
-				
-			}
-			BeanHelper.info(sessionBean.getMessage("success_collection_create"));
+		if(createdCollection())
 			FacesContext.getCurrentInstance().getExternalContext().redirect(navigation.getCollectionUrl()+ getCollection().getIdString());
-			return "";
-		} else
-			return "";
+		return "";
 	}
 	/**
 	 * Method for save&editProfile button. Create the {@link CollectionImeji} according to
@@ -147,9 +127,48 @@ public class CreateCollectionBean extends CollectionBean {
 	 * @throws Exception
 	 */  
 	public String saveAndEditProfile() throws Exception {
+		if(createdCollection());
+			FacesContext.getCurrentInstance().getExternalContext().redirect(navigation.getProfileUrl()+extractIDFromURI(getCollection().getProfile())+"/edit?init=1&col="+ getCollection().getIdString());
 		return "";
 	}
 	
+	public boolean createdCollection() throws ImejiException, URISyntaxException{
+		if(valid()){
+		CollectionController collectionController = new CollectionController();
+        int pos = 0;
+        // Set the position of the statement (used for the sorting later)
+        for (Person p : getCollection().getMetadata().getPersons())
+        {
+            p.setPos(pos);
+            pos++;
+            int pos2 = 0;
+            for(Organization o : p.getOrganizations()){
+            	o.setPos(pos2);
+            	pos2++;	            	
+            }
+        }  
+        URI id = collectionController.create(getCollection(), null, sessionBean.getUser());
+        setCollection(collectionController.retrieve(id, sessionBean.getUser()));
+        getProfile().setId(new URI(extractIDFromURI(getCollection().getProfile())));
+		BeanHelper.info(sessionBean.getMessage("success_collection_create"));
+		if(isUseMDProfileTemplate()){
+			String profileTitle = getProfile().getTitle();
+			ProfileController pc = new ProfileController();
+			this.setProfile(pc.retrieve(extractIDFromURI(getCollection().getProfile()), sessionBean.getUser()));
+			this.getProfile().setTitle(profileTitle);
+			this.getProfile().setStatements(getProfileTemplate().getStatements());
+			pc.update(getProfile(), sessionBean.getUser());
+			
+		}
+        return true;
+		}
+		return false;
+		
+	}
+	
+	public static String extractIDFromURI(URI uri) {
+		return uri.getPath().substring(uri.getPath().lastIndexOf("/") + 1);
+	}
 
 	/**
 	 * Return the link for the Cancel button

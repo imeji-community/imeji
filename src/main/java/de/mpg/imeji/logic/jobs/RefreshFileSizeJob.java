dacc05db18d39f3b8e03a11e3ca459e9c1e029d8
Bastien Saquet <saquet@mpdl.mpg.de>
2015-03-13 15:58:36 +0100

    Add job (thread) to refresh the file size of all items

---
+++ src/main/java/de/mpg/imeji/logic/jobs/RefreshFileSizeJob.java
@@ -0,0 +1,55 @@
+package de.mpg.imeji.logic.jobs;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import org.apache.log4j.Logger;
+
+import de.mpg.imeji.exceptions.ImejiException;
+import de.mpg.imeji.logic.Imeji;
+import de.mpg.imeji.logic.controller.ItemController;
+import de.mpg.imeji.logic.storage.internal.InternalStorageManager;
+import de.mpg.imeji.logic.vo.Item;
+
+/**
+ * Job which read all Items, read for each {@link Item} the size of the original
+ * File, and write the items in Jena back with the file size;
+ * 
+ * @author saquet
+ *
+ */
+public class RefreshFileSizeJob implements Callable<Integer> {
+	private static Logger logger = Logger.getLogger(RefreshFileSizeJob.class);
+
+
+	@Override
+	public Integer call() throws Exception {
+		logger.info("Starting refreshing the file size of all Items");
+		ItemController itemController = new ItemController();
+		InternalStorageManager storageManager = new InternalStorageManager();
+		logger.info("Retrieving all items...");
+		Collection<Item> items = itemController.retrieveAll(Imeji.adminUser);
+		logger.info("...done (found  " + items.size() + ")");
+		logger.info("Reading the original file of each item:");
+		int count = 1;
+		for (Item item : items) {
+			logger.info(count + "/" + items.size());
+			String path = storageManager.transformUrlToPath(item
+					.getFullImageUrl().toString());
+			File f = new File(path);
+			item.setFileSize(f.length());
+			count++;
+		}
+		logger.info("Updating the Items with the file size...");
+		try {
+			itemController.update(items, Imeji.adminUser);
+		} catch (ImejiException e) {
+			logger.error(e);
+		}
+		logger.info("...done");
+		return 1;
+	}
+}
--- src/main/java/de/mpg/imeji/presentation/admin/AdminBean.java
+++ src/main/java/de/mpg/imeji/presentation/admin/AdminBean.java
@@ -21,6 +21,7 @@ import de.mpg.imeji.logic.Imeji;
 import de.mpg.imeji.logic.ImejiSPARQL;
 import de.mpg.imeji.logic.controller.ItemController;
 import de.mpg.imeji.logic.controller.UserController;
+import de.mpg.imeji.logic.jobs.RefreshFileSizeJob;
 import de.mpg.imeji.logic.reader.ReaderFacade;
 import de.mpg.imeji.logic.search.Search;
 import de.mpg.imeji.logic.search.Search.SearchType;
@@ -51,345 +52,333 @@ import de.mpg.j2j.annotations.j2jId;
  * @author $Author$ (last modification)
  * @version $Revision$ $LastChangedDate$
  */
-public class AdminBean
-{
-    private SessionBean sb;
-    private static Logger logger = Logger.getLogger(AdminBean.class);
-    private boolean clean = false;
+public class AdminBean {
+	private SessionBean sb;
+	private static Logger logger = Logger.getLogger(AdminBean.class);
+	private boolean clean = false;
 
-    public AdminBean()
-    {
-        sb = (SessionBean)BeanHelper.getSessionBean(SessionBean.class);
-    }
+	public AdminBean() {
+		sb = (SessionBean) BeanHelper.getSessionBean(SessionBean.class);
+	}
 
-    /**
-     * Import the files in an external storage (for instance escidoc) into the internal storage
-     * 
-     * @throws Exception
-     */
-    public String importToInternalStorage() throws Exception
-    {
-        StorageController internal = new StorageController("internal");
-        StorageController escidoc = new StorageController("escidoc");
-        ItemController ic = new ItemController();
-        for (Item item : ic.retrieveAll(sb.getUser()))
-        {
-            File tmp = null;
-            try
-            {
-                // Get escidoc url for all files
-                URI escidocUrl = item.getFullImageUrl();
-                logger.info("Importing file " + escidocUrl + " for item " + item.getId());
-                ByteArrayOutputStream out = new ByteArrayOutputStream();
-                // Read the file in a stream
-                escidoc.read(escidocUrl.toString(), out, true);
-                // Upload the file in the internal storage
-                if (out.toByteArray() != null)
-                {
-                    tmp = File.createTempFile("import", FilenameUtils.getExtension(item.getFilename()));
-                    FileUtils.writeByteArrayToFile(tmp, out.toByteArray());
-                    UploadResult result = internal.upload(item.getFilename(), tmp,
-                            ObjectHelper.getId(item.getCollection()));
-                    FileUtils.deleteQuietly(tmp);
-                    item.setChecksum(result.getChecksum());
-                    item.setFullImageUrl(URI.create(result.getOrginal()));
-                    item.setWebImageUrl(URI.create(result.getWeb()));
-                    item.setThumbnailImageUrl(URI.create(result.getThumb()));
-                    item.setStorageId(result.getId());
-                    item.setFiletype(item.getFiletype());
-                    // Update the item with the new values
-                    ic.update(item, sb.getUser());
-                }
-                else
-                {
-                    logger.error("File not found: " + escidocUrl + " for item " + item.getId());
-                }
-            }
-            catch (Exception e)
-            {
-                logger.error("Error importing item " + item.getId(), e);
-            }
-            finally
-            {
-                FileUtils.deleteQuietly(tmp);
-            }
-        }
-        return "";
-    }
+	/**
+	 * Import the files in an external storage (for instance escidoc) into the
+	 * internal storage
+	 * 
+	 * @throws Exception
+	 */
+	public String importToInternalStorage() throws Exception {
+		StorageController internal = new StorageController("internal");
+		StorageController escidoc = new StorageController("escidoc");
+		ItemController ic = new ItemController();
+		for (Item item : ic.retrieveAll(sb.getUser())) {
+			File tmp = null;
+			try {
+				// Get escidoc url for all files
+				URI escidocUrl = item.getFullImageUrl();
+				logger.info("Importing file " + escidocUrl + " for item "
+						+ item.getId());
+				ByteArrayOutputStream out = new ByteArrayOutputStream();
+				// Read the file in a stream
+				escidoc.read(escidocUrl.toString(), out, true);
+				// Upload the file in the internal storage
+				if (out.toByteArray() != null) {
+					tmp = File.createTempFile("import",
+							FilenameUtils.getExtension(item.getFilename()));
+					FileUtils.writeByteArrayToFile(tmp, out.toByteArray());
+					UploadResult result = internal.upload(item.getFilename(),
+							tmp, ObjectHelper.getId(item.getCollection()));
+					FileUtils.deleteQuietly(tmp);
+					item.setChecksum(result.getChecksum());
+					item.setFullImageUrl(URI.create(result.getOrginal()));
+					item.setWebImageUrl(URI.create(result.getWeb()));
+					item.setThumbnailImageUrl(URI.create(result.getThumb()));
+					item.setStorageId(result.getId());
+					item.setFiletype(item.getFiletype());
+					// Update the item with the new values
+					ic.update(item, sb.getUser());
+				} else {
+					logger.error("File not found: " + escidocUrl + " for item "
+							+ item.getId());
+				}
+			} catch (Exception e) {
+				logger.error("Error importing item " + item.getId(), e);
+			} finally {
+				FileUtils.deleteQuietly(tmp);
+			}
+		}
+		return "";
+	}
 
-    /**
-     * Clean the {@link Storage}
-     * 
-     * @return
-     */
-    public String cleanStorage()
-    {
-        StorageController controller = new StorageController();
-        controller.getAdministrator().clean();
-        return "pretty:";
-    }
+	/**
+	 * Refresh the file size of all items
+	 * 
+	 * @return
+	 */
+	public String refreshFileSize() {
+		Imeji.executor.submit(new RefreshFileSizeJob());
+		return "";
+	}
 
-    /**
-     * Return the location of the internal storage
-     * 
-     * @return
-     * @throws IOException
-     * @throws URISyntaxException
-     */
-    public String getInternalStorageLocation() throws IOException, URISyntaxException
-    {
-        return PropertyReader.getProperty("imeji.storage.path");
-    }
+	/**
+	 * Clean the {@link Storage}
+	 * 
+	 * @return
+	 */
+	public String cleanStorage() {
+		StorageController controller = new StorageController();
+		controller.getAdministrator().clean();
+		return "pretty:";
+	}
 
-    /**
-     * Initialize the full text index for all elements
-     * 
-     * @throws Exception
-     */
-    public void reIndex() throws Exception
-    {
-    }
+	/**
+	 * Return the location of the internal storage
+	 * 
+	 * @return
+	 * @throws IOException
+	 * @throws URISyntaxException
+	 */
+	public String getInternalStorageLocation() throws IOException,
+			URISyntaxException {
+		return PropertyReader.getProperty("imeji.storage.path");
+	}
 
-    /**
-     * Make the same as clean, but doesn't remove the resources
-     * 
-     * @throws Exception
-     */
-    public void status() throws Exception
-    {
-        clean = false;
-        invokeCleanMethods();
-    }
+	/**
+	 * Initialize the full text index for all elements
+	 * 
+	 * @throws Exception
+	 */
+	public void reIndex() throws Exception {
+	}
 
-    /**
-     * Here are called all methods related to data cleaning
-     * 
-     * @throws Exception
-     */
-    public void clean() throws Exception
-    {
-        clean = true;
-        invokeCleanMethods();
-        reIndex();
-    }
+	/**
+	 * Make the same as clean, but doesn't remove the resources
+	 * 
+	 * @throws Exception
+	 */
+	public void status() throws Exception {
+		clean = false;
+		invokeCleanMethods();
+	}
 
-    /**
-     * Invoke all clean methods available
-     * 
-     * @throws Exception
-     */
-    private void invokeCleanMethods() throws Exception
-    {
-        cleanStatement();
-        /*
-         * TODO Clean Metadata not working: the metadata is not completely removed. All element in the metadata are
-         * removed, but the metadata it self not. Since a metadata is a abstract class, j2j can not instance a new
-         * metadata since it doesn't know the type
-         */
-        // cleanMetadata();
-        cleanGrants();
-    }
+	/**
+	 * Here are called all methods related to data cleaning
+	 * 
+	 * @throws Exception
+	 */
+	public void clean() throws Exception {
+		clean = true;
+		invokeCleanMethods();
+		reIndex();
+	}
 
-    /**
-     * Clean {@link Statement} which are not bound a {@link MetadataProfile}
-     * 
-     * @throws Exception
-     */
-    private void cleanStatement() throws Exception
-    {
-        logger.info("Searching not bounded statement...");
-        Search search = SearchFactory.create();
-        List<String> uris = search.searchSimpleForQuery(SPARQLQueries.selectStatementUnbounded()).getResults();
-        logger.info("...found " + uris.size());
-        removeResources(uris, Imeji.profileModel, new Statement());
-    }
+	/**
+	 * Invoke all clean methods available
+	 * 
+	 * @throws Exception
+	 */
+	private void invokeCleanMethods() throws Exception {
+		cleanStatement();
+		/*
+		 * TODO Clean Metadata not working: the metadata is not completely
+		 * removed. All element in the metadata are removed, but the metadata it
+		 * self not. Since a metadata is a abstract class, j2j can not instance
+		 * a new metadata since it doesn't know the type
+		 */
+		// cleanMetadata();
+		cleanGrants();
+	}
 
-    /**
-     * Clean grants which are not related to a user
-     * 
-     * @throws Exception
-     */
-    private void cleanGrants() throws Exception
-    {
-        logger.info("Searching not bounded grants...");
-        Search search = SearchFactory.create();
-        List<String> uris = search.searchSimpleForQuery(SPARQLQueries.selectGrantWithoutUser()).getResults();
-        logger.info("...found " + uris.size());
-        System.out.println(uris.size());
-        removeResources(uris, Imeji.userModel, new Grant());
-        logger.info("Searching broken grants...");
-        uris = search.searchSimpleForQuery(SPARQLQueries.selectGrantBroken()).getResults();
-        logger.info("...found " + uris.size());
-        System.out.println(uris.size());
-        removeResources(uris, Imeji.userModel, new Grant());
-        logger.info("Searching emtpy grants...");
-        if (clean)
-            ImejiSPARQL.execUpdate(SPARQLQueries.removeGrantEmtpy());
-        uris = search.searchSimpleForQuery(SPARQLQueries.selectGrantEmtpy()).getResults();
-        logger.info("...found " + uris.size());
-        System.out.println(uris.size());
-    }
+	/**
+	 * Clean {@link Statement} which are not bound a {@link MetadataProfile}
+	 * 
+	 * @throws Exception
+	 */
+	private void cleanStatement() throws Exception {
+		logger.info("Searching not bounded statement...");
+		Search search = SearchFactory.create();
+		List<String> uris = search.searchSimpleForQuery(
+				SPARQLQueries.selectStatementUnbounded()).getResults();
+		logger.info("...found " + uris.size());
+		removeResources(uris, Imeji.profileModel, new Statement());
+	}
 
-    /**
-     * Remove Exception a {@link List} of {@link Resource}
-     * 
-     * @param uris
-     * @param modelName
-     * @throws IllegalAccessException
-     * @throws InstantiationException
-     * @throws Exception
-     */
-    private synchronized void removeResources(List<String> uris, String modelName, Object obj)
-            throws InstantiationException, IllegalAccessException, Exception
-    {
-        removeObjects(loadResourcesAsObjects(uris, modelName, obj), modelName);
-    }
+	/**
+	 * Clean grants which are not related to a user
+	 * 
+	 * @throws Exception
+	 */
+	private void cleanGrants() throws Exception {
+		logger.info("Searching not bounded grants...");
+		Search search = SearchFactory.create();
+		List<String> uris = search.searchSimpleForQuery(
+				SPARQLQueries.selectGrantWithoutUser()).getResults();
+		logger.info("...found " + uris.size());
+		System.out.println(uris.size());
+		removeResources(uris, Imeji.userModel, new Grant());
+		logger.info("Searching broken grants...");
+		uris = search.searchSimpleForQuery(SPARQLQueries.selectGrantBroken())
+				.getResults();
+		logger.info("...found " + uris.size());
+		System.out.println(uris.size());
+		removeResources(uris, Imeji.userModel, new Grant());
+		logger.info("Searching emtpy grants...");
+		if (clean)
+			ImejiSPARQL.execUpdate(SPARQLQueries.removeGrantEmtpy());
+		uris = search.searchSimpleForQuery(SPARQLQueries.selectGrantEmtpy())
+				.getResults();
+		logger.info("...found " + uris.size());
+		System.out.println(uris.size());
+	}
 
-    /**
-     * Load the {@link Resource} as {@link Object}
-     * 
-     * @param uris
-     * @param modelName
-     * @param obj
-     * @return
-     */
-    private List<Object> loadResourcesAsObjects(List<String> uris, String modelName, Object obj)
-    {
-        ReaderFacade reader = new ReaderFacade(modelName);
-        List<Object> l = new ArrayList<Object>();
-        for (String uri : uris)
-        {
-            try
-            {
-                logger.info("Resource to be removed: " + uri);
-                l.add(reader.read(uri, sb.getUser(), obj.getClass().newInstance()));
-            }
-            catch (Exception e)
-            {
-                logger.error("ERROR LOADING RESOURCE " + uri + " !!!!!", e);
-            }
-        }
-        return l;
-    }
+	/**
+	 * Remove Exception a {@link List} of {@link Resource}
+	 * 
+	 * @param uris
+	 * @param modelName
+	 * @throws IllegalAccessException
+	 * @throws InstantiationException
+	 * @throws Exception
+	 */
+	private synchronized void removeResources(List<String> uris,
+			String modelName, Object obj) throws InstantiationException,
+			IllegalAccessException, Exception {
+		removeObjects(loadResourcesAsObjects(uris, modelName, obj), modelName);
+	}
 
-    /**
-     * Remove an {@link Object}, it must have a {@link j2jId}
-     * 
-     * @param l
-     * @param modelName
-     * @throws Exception
-     */
-    private void removeObjects(List<Object> l, String modelName) throws Exception
-    {
-        if (clean)
-        {
-            WriterFacade writer = new WriterFacade(modelName);
-            writer.delete(l, sb.getUser());
-        }
-    }
+	/**
+	 * Load the {@link Resource} as {@link Object}
+	 * 
+	 * @param uris
+	 * @param modelName
+	 * @param obj
+	 * @return
+	 */
+	private List<Object> loadResourcesAsObjects(List<String> uris,
+			String modelName, Object obj) {
+		ReaderFacade reader = new ReaderFacade(modelName);
+		List<Object> l = new ArrayList<Object>();
+		for (String uri : uris) {
+			try {
+				logger.info("Resource to be removed: " + uri);
+				l.add(reader.read(uri, sb.getUser(), obj.getClass()
+						.newInstance()));
+			} catch (Exception e) {
+				logger.error("ERROR LOADING RESOURCE " + uri + " !!!!!", e);
+			}
+		}
+		return l;
+	}
 
-    /**
-     * return count of all {@link Album}
-     * 
-     * @return
-     */
-    public int getAllAlbumsSize()
-    {
-        Search search = SearchFactory.create(SearchType.ALBUM);
-        return search.searchSimpleForQuery(SPARQLQueries.selectAlbumAll()).getNumberOfRecords();
-    }
+	/**
+	 * Remove an {@link Object}, it must have a {@link j2jId}
+	 * 
+	 * @param l
+	 * @param modelName
+	 * @throws Exception
+	 */
+	private void removeObjects(List<Object> l, String modelName)
+			throws Exception {
+		if (clean) {
+			WriterFacade writer = new WriterFacade(modelName);
+			writer.delete(l, sb.getUser());
+		}
+	}
 
-    /**
-     * return count of all {@link CollectionImeji}
-     * 
-     * @return
-     */
-    public int getAllCollectionsSize()
-    {
-        Search search = SearchFactory.create(SearchType.COLLECTION);
-        return search.searchSimpleForQuery(SPARQLQueries.selectCollectionAll()).getNumberOfRecords();
-    }
+	/**
+	 * return count of all {@link Album}
+	 * 
+	 * @return
+	 */
+	public int getAllAlbumsSize() {
+		Search search = SearchFactory.create(SearchType.ALBUM);
+		return search.searchSimpleForQuery(SPARQLQueries.selectAlbumAll())
+				.getNumberOfRecords();
+	}
 
-    /**
-     * return count of all {@link Item}
-     * 
-     * @return
-     */
-    public int getAllImagesSize()
-    {
-        Search search = SearchFactory.create(SearchType.ITEM);
-        return search.searchSimpleForQuery(SPARQLQueries.selectItemAll()).getNumberOfRecords();
-    }
+	/**
+	 * return count of all {@link CollectionImeji}
+	 * 
+	 * @return
+	 */
+	public int getAllCollectionsSize() {
+		Search search = SearchFactory.create(SearchType.COLLECTION);
+		return search.searchSimpleForQuery(SPARQLQueries.selectCollectionAll())
+				.getNumberOfRecords();
+	}
 
-    /**
-     * True if the current {@link Storage} has implemted a {@link StorageAdministrator}
-     * 
-     * @return
-     */
-    public boolean isAdministrate()
-    {
-        StorageController sc = new StorageController();
-        return sc.getAdministrator() != null;
-    }
+	/**
+	 * return count of all {@link Item}
+	 * 
+	 * @return
+	 */
+	public int getAllImagesSize() {
+		Search search = SearchFactory.create(SearchType.ITEM);
+		return search.searchSimpleForQuery(SPARQLQueries.selectItemAll())
+				.getNumberOfRecords();
+	}
 
-    /**
-     * Return the number of files stored in the {@link Storage}
-     * 
-     * @return
-     */
-    public int getNumberOfFiles()
-    {
-        StorageController sc = new StorageController();
-        return (int)sc.getAdministrator().getNumberOfFiles();
-    }
+	/**
+	 * True if the current {@link Storage} has implemted a
+	 * {@link StorageAdministrator}
+	 * 
+	 * @return
+	 */
+	public boolean isAdministrate() {
+		StorageController sc = new StorageController();
+		return sc.getAdministrator() != null;
+	}
 
-    /**
-     * Return the free space in the {@link Storage} in bytes
-     * 
-     * @return
-     */
-    public long getStorageFreeSpace()
-    {
-        StorageController sc = new StorageController();
-        return sc.getAdministrator().getFreeSpace();
-    }
+	/**
+	 * Return the number of files stored in the {@link Storage}
+	 * 
+	 * @return
+	 */
+	public int getNumberOfFiles() {
+		StorageController sc = new StorageController();
+		return (int) sc.getAdministrator().getNumberOfFiles();
+	}
 
-    /**
-     * Return the Size in bytes of all files in the {@link Storage}
-     * 
-     * @return
-     */
-    public long getSizeOfFiles()
-    {
-        StorageController sc = new StorageController();
-        return sc.getAdministrator().getSizeOfFiles();
-    }
+	/**
+	 * Return the free space in the {@link Storage} in bytes
+	 * 
+	 * @return
+	 */
+	public long getStorageFreeSpace() {
+		StorageController sc = new StorageController();
+		return sc.getAdministrator().getFreeSpace();
+	}
 
-    /**
-     * Return all {@link User}
-     * 
-     * @return
-     */
-    public List<User> getAllUsers()
-    {
-        UserController uc = new UserController(Imeji.adminUser);
-        return (List<User>)uc.searchUserByName("");
-    }
+	/**
+	 * Return the Size in bytes of all files in the {@link Storage}
+	 * 
+	 * @return
+	 */
+	public long getSizeOfFiles() {
+		StorageController sc = new StorageController();
+		return sc.getAdministrator().getSizeOfFiles();
+	}
 
-    /**
-     * return count of all {@link User}
-     * 
-     * @return
-     */
-    public int getAllUsersSize()
-    {
-        try
-        {
-            return this.getAllUsers().size();
-        }
-        catch (Exception e)
-        {
-            return 0;
-        }
-    }
+	/**
+	 * Return all {@link User}
+	 * 
+	 * @return
+	 */
+	public List<User> getAllUsers() {
+		UserController uc = new UserController(Imeji.adminUser);
+		return (List<User>) uc.searchUserByName("");
+	}
+
+	/**
+	 * return count of all {@link User}
+	 * 
+	 * @return
+	 */
+	public int getAllUsersSize() {
+		try {
+			return this.getAllUsers().size();
+		} catch (Exception e) {
+			return 0;
+		}
+	}
 }

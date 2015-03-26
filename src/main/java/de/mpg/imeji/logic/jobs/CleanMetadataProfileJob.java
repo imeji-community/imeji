9b1db52659e9273aaa2fd66d1899b5b7dc259fce
Bastien Saquet <saquet@mpdl.mpg.de>
2015-03-26 14:43:46 +0100

    Find unused profile plus fix java2jena cast problem
    fix: java.util.LinkedList cannot be cast to java.util.ArrayList

---
+++ src/main/java/de/mpg/imeji/logic/jobs/CleanMetadataProfileJob.java
@@ -0,0 +1,52 @@
+package de.mpg.imeji.logic.jobs;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import de.mpg.imeji.logic.Imeji;
+import de.mpg.imeji.logic.controller.ProfileController;
+import de.mpg.imeji.logic.search.SPARQLSearch;
+import de.mpg.imeji.logic.search.Search;
+import de.mpg.imeji.logic.search.Search.SearchType;
+import de.mpg.imeji.logic.search.query.SPARQLQueries;
+import de.mpg.imeji.logic.vo.MetadataProfile;
+
+/**
+ * Clean unused {@link MetadataProfile}
+ * 
+ * @author saquet
+ *
+ */
+public class CleanMetadataProfileJob implements Callable<Integer> {
+
+	private boolean delete = false;
+	private List<MetadataProfile> profiles = new ArrayList<MetadataProfile>();
+
+	public CleanMetadataProfileJob(boolean delete) {
+		this.delete = delete;
+	}
+
+	@Override
+	public Integer call() throws Exception {
+		Search s = new SPARQLSearch(SearchType.ALL, null);
+		List<String> r = s.searchSimpleForQuery(
+				SPARQLQueries.selectUnusedMetadataProfiles()).getResults();
+		ProfileController pc = new ProfileController();
+		for (String uri : r) {
+			profiles.add(pc.retrieve(URI.create(uri), Imeji.adminUser));
+		}
+		if (delete) {
+			for (MetadataProfile mdp : profiles) {
+				pc.delete(mdp, Imeji.adminUser);
+			}
+		}
+		return 1;
+	}
+
+	public List<MetadataProfile> getProfiles() {
+		return profiles;
+	}
+
+}

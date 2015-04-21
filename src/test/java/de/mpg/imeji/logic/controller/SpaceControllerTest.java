71372a65921240103f9e730e289f7ecb55755517
Vladislav Makarenko <makarenko@mpdl.mpg.de>
2015-04-21 17:59:08 +0200

    initial space performance test

--- src/test/java/de/mpg/imeji/logic/controller/SpaceControllerTest.java
+++ src/test/java/de/mpg/imeji/logic/controller/SpaceControllerTest.java
@@ -3,10 +3,14 @@ package de.mpg.imeji.logic.controller;
 import com.google.common.collect.Iterables;
 import de.mpg.imeji.logic.vo.Space;
 import de.mpg.imeji.presentation.util.ImejiFactory;
+import de.mpg.imeji.rest.api.CollectionService;
+import de.mpg.imeji.rest.process.RestProcessUtils;
 import de.mpg.imeji.rest.resources.test.integration.ImejiTestBase;
+import de.mpg.imeji.rest.to.CollectionTO;
 import org.apache.commons.io.FileUtils;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 import org.slf4j.Logger;
@@ -16,6 +20,8 @@ import java.io.File;
 import java.net.URI;
 
 import static de.mpg.imeji.logic.Imeji.adminUser;
+import static de.mpg.imeji.logic.util.ResourceHelper.getStringFromPath;
+import static de.mpg.imeji.rest.resources.test.integration.MyTestContainerFactory.STATIC_CONTEXT_REST;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
@@ -29,6 +35,7 @@ public class SpaceControllerTest extends ImejiTestBase{
 
     private static final Logger LOGGER = LoggerFactory
             .getLogger(SpaceControllerTest.class);
+    public static final int COL_NUM = 10;
 
     private static SpaceController sc;
     private static CollectionController cc;
@@ -61,6 +68,7 @@ public class SpaceControllerTest extends ImejiTestBase{
         sc.addCollection(space, collId2, adminUser);
         //duplicates are not added!
         sc.addCollection(space, collId2, adminUser);
+
         assertThat(space.getTitle(), endsWith(changed));
         assertThat(sc.retrieveCollections(space), hasSize(2));
     }
@@ -109,4 +117,39 @@ public class SpaceControllerTest extends ImejiTestBase{
         }
         assertThat(sc.retrieveAll(), empty());
     }
+
+    @Ignore
+    @Test
+    public void test_8_Performance() throws Exception {
+
+
+        //create space
+        Space sp1 = ImejiFactory.newSpace();
+        sp1.setTitle("Space 1");
+        spaceId = sc.create(sp1, adminUser);
+        space = sc.retrieve(sp1.getId(), adminUser);
+        assertThat(sp1.getTitle(), equalTo(space.getTitle()));
+
+        //create COL_NUM collections
+        String[] colIds = new String[COL_NUM];
+        collectionTO= (CollectionTO) RestProcessUtils.buildTOFromJSON(
+                getStringFromPath(STATIC_CONTEXT_REST + "/createCollection.json"), CollectionTO.class);
+        CollectionService cs = new CollectionService();
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < COL_NUM; i++) {
+            collectionTO.setTitle("Collection " + i);
+            colIds[i] = cs.create(collectionTO, testUser).getId();
+        }
+        LOGGER.info("creation time: " + (System.currentTimeMillis() - startTime ) + "ms");
+
+        startTime = System.currentTimeMillis();
+        //add collections to space
+        for (int i = 0; i < COL_NUM; i++) {
+            sc.addCollection(space, colIds[i], adminUser);
+        }
+        LOGGER.info("addition time:" + (System.currentTimeMillis() - startTime )  + "ms");
+
+    }
+
+
 }

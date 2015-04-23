0e44fb51d6b2600e3142ef41d3f5bc91326f33d0
cafefish <yu@mpdl.mpg.de>
2015-04-23 11:57:22 +0200

    update create space

--- src/main/java/de/mpg/imeji/presentation/space/CreateSpaceBean.java
+++ src/main/java/de/mpg/imeji/presentation/space/CreateSpaceBean.java
@@ -1,7 +1,26 @@
 package de.mpg.imeji.presentation.space;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
+import javax.servlet.ServletContext;
+import javax.servlet.http.Part;
+
 import com.google.common.io.ByteStreams;
 import com.google.common.io.Files;
+
 import de.mpg.imeji.exceptions.ImejiException;
 import de.mpg.imeji.logic.controller.CollectionController;
 import de.mpg.imeji.logic.controller.SpaceController;
@@ -12,18 +31,6 @@ import de.mpg.imeji.presentation.beans.Navigation;
 import de.mpg.imeji.presentation.session.SessionBean;
 import de.mpg.imeji.presentation.util.BeanHelper;
 import de.mpg.imeji.rest.process.CommonUtils;
-
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
-import javax.faces.context.FacesContext;
-import javax.faces.model.SelectItem;
-import javax.servlet.ServletContext;
-import javax.servlet.http.Part;
-import java.io.*;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
 
 @ManagedBean(name ="CreateSpaceBean")
 @ViewScoped
@@ -86,14 +93,14 @@ public class CreateSpaceBean implements Serializable{
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
-    	
+    	File tmpFile = null; 
     	if(logoFile != null)
     	{
     		try {
 				InputStream inputStream = logoFile.getInputStream();
 	    		ServletContext servletContext = (ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext();
 	    		File tmpPath = (File)servletContext.getAttribute(CommonUtils.JAVAX_SERVLET_CONTEXT_TEMPDIR);
-	    		File tmpFile = File.createTempFile("spaceLogo", "." + Files.getFileExtension(logoFile.getName()), tmpPath);
+	    		tmpFile = File.createTempFile("spaceLogo", "." + Files.getFileExtension(logoFile.getName()), tmpPath);
 	    		ByteStreams.copy(inputStream, new FileOutputStream(tmpFile));
 	    		//...Upload To Server
 	    		
@@ -108,12 +115,13 @@ public class CreateSpaceBean implements Serializable{
     	SpaceController spaceController = new SpaceController();
     	try {
 			spaceController.validate(space, sessionBean.getUser());
-	    	URI uri = spaceController.create(space, sessionBean.getUser());        
+			Collection<String> collections = new ArrayList<String>(selectedCollections);
+	    	URI uri = spaceController.create(space, collections, tmpFile, sessionBean.getUser());
 	    	setSpace(spaceController.retrieve(uri, sessionBean.getUser()));
-		} catch (ImejiException e) {
+		} catch (ImejiException|IOException e) {
 			BeanHelper.error(sessionBean.getMessage(e.getMessage()));
 			return false;
-		}
+		} 
         
         /*
          * Update User Grant

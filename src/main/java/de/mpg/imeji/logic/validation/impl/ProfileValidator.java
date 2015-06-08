0e878795a585c10fa564bf4e4430773319148801
FelsyWaschbaer <Felsy_Waschbaer@web.de>
2015-06-08 12:18:02 +0200

    Validate uniqueness of metadata labels

--- src/main/java/de/mpg/imeji/logic/validation/impl/ProfileValidator.java
+++ src/main/java/de/mpg/imeji/logic/validation/impl/ProfileValidator.java
@@ -2,6 +2,8 @@ package de.mpg.imeji.logic.validation.impl;
 
 import static com.google.common.base.Strings.isNullOrEmpty;
 
+import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
 
 import de.mpg.imeji.exceptions.UnprocessableError;
@@ -27,10 +29,20 @@ public class ProfileValidator implements Validator<MetadataProfile> {
 			throw new UnprocessableError("error_profile_need_statement");
 		}
 		int i = 0;
+		
+		//helper hashmap to validate uniqueness of metadata labels
+		HashMap<String, URI> labels = new HashMap<>();
+		
 		for (Statement s : profile.getStatements()) {
 			for (LocalizedString ls : s.getLabels()) {
 				if (ls.getLang() == null || "".equals(ls.getLang())) {
 					throw new UnprocessableError("error_profile_label_no_lang");
+				}
+				//validate uniqueness of metadata labels
+				if(labels.containsKey(ls.getValue()) && !labels.get(ls.getValue()).equals(s.getId())){
+					throw new UnprocessableError("labels_have_to_be_unique");
+				}else{
+					labels.put(ls.getValue(), s.getId());
 				}
 			}
 			if (s.getType() == null) {

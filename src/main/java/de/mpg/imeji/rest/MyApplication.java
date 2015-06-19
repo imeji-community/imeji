7f2acad93ddbf3507a946ae773be5c14562cfe85
Bastien Saquet <saquet@mpdl.mpg.de>
2015-06-19 17:58:38 +0200

    Jersey logger only in debug modus

--- src/main/java/de/mpg/imeji/rest/MyApplication.java
+++ src/main/java/de/mpg/imeji/rest/MyApplication.java
@@ -1,20 +1,24 @@
 package de.mpg.imeji.rest;
 
+import org.apache.log4j.Logger;
 import org.glassfish.jersey.filter.LoggingFilter;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class MyApplication extends ResourceConfig {
+	Logger logger = Logger.getLogger(MyApplication.class);
 
-    public MyApplication() {
-        packages("de.mpg.imeji.rest.resources");
-        //Uncomment loggingFilter registration below to start logging your service requests
-        register(LoggingFilter.class);
-        register(MultiPartFeature.class);
+	public MyApplication() {
+		packages("de.mpg.imeji.rest.resources");
+		// Uncomment loggingFilter registration below to start logging your
+		// service requests
+		if (logger.isDebugEnabled()) {
+			register(LoggingFilter.class);
+		}
+		register(MultiPartFeature.class);
 
-//        register(MoxyJsonFeature.class);
-//        property(MarshallerProperties.JSON_NAMESPACE_SEPARATOR, ".");
-  }
-
+		// register(MoxyJsonFeature.class);
+		// property(MarshallerProperties.JSON_NAMESPACE_SEPARATOR, ".");
+	}
 
 }

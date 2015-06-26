2fbc802ccfa3a66250f007160a52576935eb7f87
Bastien Saquet <saquet@mpdl.mpg.de>
2015-06-26 15:01:32 +0200

    #176 Minimize stacktrace for some exceptions

--- src/main/java/de/mpg/imeji/exceptions/NotAllowedError.java
+++ src/main/java/de/mpg/imeji/exceptions/NotAllowedError.java
@@ -1,47 +1,43 @@
 /*
- *
+ * 
  * CDDL HEADER START
- *
- * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License"). You may not use this file except in compliance
- * with the License.
- *
- * You can obtain a copy of the license at license/ESCIDOC.LICENSE
- * or http://www.escidoc.de/license.
- * See the License for the specific language governing permissions
- * and limitations under the License.
- *
- * When distributing Covered Code, include this CDDL HEADER in each
- * file and include the License file at license/ESCIDOC.LICENSE.
- * If applicable, add the following below this CDDL HEADER, with the
- * fields enclosed by brackets "[]" replaced with your own identifying
- * information: Portions Copyright [yyyy] [name of copyright owner]
- *
+ * 
+ * The contents of this file are subject to the terms of the Common Development and Distribution
+ * License, Version 1.0 only (the "License"). You may not use this file except in compliance with
+ * the License.
+ * 
+ * You can obtain a copy of the license at license/ESCIDOC.LICENSE or http://www.escidoc.de/license.
+ * See the License for the specific language governing permissions and limitations under the
+ * License.
+ * 
+ * When distributing Covered Code, include this CDDL HEADER in each file and include the License
+ * file at license/ESCIDOC.LICENSE. If applicable, add the following below this CDDL HEADER, with
+ * the fields enclosed by brackets "[]" replaced with your own identifying information: Portions
+ * Copyright [yyyy] [name of copyright owner]
+ * 
  * CDDL HEADER END
  */
 /*
- * Copyright 2006-2007 Fachinformationszentrum Karlsruhe Gesellschaft
- * für wissenschaftlich-technische Information mbH and Max-Planck-
- * Gesellschaft zur Förderung der Wissenschaft e.V.
- * All rights reserved. Use is subject to license terms.
+ * Copyright 2006-2007 Fachinformationszentrum Karlsruhe Gesellschaft für
+ * wissenschaftlich-technische Information mbH and Max-Planck- Gesellschaft zur Förderung der
+ * Wissenschaft e.V. All rights reserved. Use is subject to license terms.
  */
 package de.mpg.imeji.exceptions;
+
 
 
 /**
  * 
  */
-public class NotAllowedError extends ImejiException
-{
-    private static final long serialVersionUID = -3504946406047760565L;
+public class NotAllowedError extends ImejiException {
+  private static final long serialVersionUID = -3504946406047760565L;
 
-    /**
-     * Constructor for HTTP 403 Forbidden
-     */
-    public NotAllowedError(String message)
-    {
-        super(message);
-    }
-    
+  /**
+   * Constructor for HTTP 403 Forbidden
+   */
+  public NotAllowedError(String message) {
+    super(message);
+    minimizeStacktrace();
+  }
+
 }
--- src/main/java/de/mpg/imeji/exceptions/NotFoundException.java
+++ src/main/java/de/mpg/imeji/exceptions/NotFoundException.java
@@ -1,11 +1,13 @@
 package de.mpg.imeji.exceptions;
 
+
 public class NotFoundException extends ImejiException {
 
-	private static final long serialVersionUID = -6006945139992063194L;
+  private static final long serialVersionUID = -6006945139992063194L;
 
-	public NotFoundException(String message) {
-		super(message,null);
-	}
+  public NotFoundException(String message) {
+    super(message, null);
+    minimizeStacktrace();
+  }
 
 }

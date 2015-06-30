ba59c8d657fe16a67a112b1c5e7908846c0f0330
Vlad Makarenko <makarenko@mpdl.mpg.de>
2015-06-30 16:03:06 +0200

    fixed ItemProcess.java:  not assigned <to> after update

--- src/test/java/de/mpg/imeji/rest/resources/test/integration/item/ItemTestSuite.java
+++ src/test/java/de/mpg/imeji/rest/resources/test/integration/item/ItemTestSuite.java
@@ -6,13 +6,13 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        ItemCreateTest.class,
-        ItemReadTest.class,
-        ItemUpdateBasicTest.class,
-        ItemUpdateFileTest.class,
-        ItemUpdateMetadataTest.class,
-        ItemDeleteTest.class,
-        ItemDefaultMdCreateTest.class,
-        ItemEasyUpdateTest.class
+        ItemCreateTest.class
+        ,ItemReadTest.class
+        ,ItemUpdateBasicTest.class
+        ,ItemUpdateFileTest.class
+        ,ItemUpdateMetadataTest.class
+        ,ItemDeleteTest.class
+        ,ItemDefaultMdCreateTest.class
+        ,ItemEasyUpdateTest.class
 })
 public class ItemTestSuite { }

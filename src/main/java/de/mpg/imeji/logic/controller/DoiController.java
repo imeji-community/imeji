38363ca5151d7d13ad9a5114f873c22f87c795da
FelsyWaschbaer <Felsy_Waschbaer@web.de>
2015-11-17 13:39:36 +0100

    Try to fix compilation error

--- src/main/java/de/mpg/imeji/logic/controller/DoiController.java
+++ src/main/java/de/mpg/imeji/logic/controller/DoiController.java
@@ -1,12 +1,11 @@
 package de.mpg.imeji.logic.controller;
 
 import java.io.StringWriter;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-import javax.net.ssl.HttpsURLConnection;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -16,32 +15,18 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 
-import org.apache.commons.httpclient.Credentials;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.UsernamePasswordCredentials;
-import org.apache.commons.httpclient.auth.AuthScope;
-import org.apache.commons.httpclient.methods.PutMethod;
-import org.apache.commons.httpclient.methods.RequestEntity;
-import org.apache.commons.httpclient.methods.StringRequestEntity;
-import org.apache.commons.httpclient.params.HttpClientParams;
 import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 
-import util.JenaUtil;
+
 import de.mpg.imeji.exceptions.ImejiException;
 import de.mpg.imeji.logic.vo.CollectionImeji;
-import de.mpg.imeji.logic.vo.Item;
 import de.mpg.imeji.logic.vo.Person;
-import de.mpg.imeji.presentation.beans.ConfigurationBean;
-import de.mpg.imeji.presentation.util.PropertyReader;
 import de.mpg.imeji.rest.doi.CollectionDO;
 import de.mpg.imeji.rest.doi.CreatorsDO;
 import de.mpg.imeji.rest.doi.IdentifierDO;
 import de.mpg.imeji.rest.doi.TitleDO;
-import de.mpg.imeji.rest.resources.test.integration.item.ItemTestBase;
-
-import javax.ws.rs.client.WebTarget;
 
 
 public class DoiController {
